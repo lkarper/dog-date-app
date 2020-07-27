ead4c736c00217ab105570cb347dff8f0c722275
Luther Karper <luther.karper@live.com>
2020-07-27 17:45:35 -0400

    Make map marker draggable

--- src/CreateHowl/CreateHowl.js
+++ src/CreateHowl/CreateHowl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Map from '../Map/Map';
 
 const CreateHowl = (props) => {
     return (
@@ -17,37 +18,37 @@ const CreateHowl = (props) => {
                         <legend>Select your dog(s) to howl about</legend>
                         <div>
                             <input type="checkbox" id="Seymour" name="Seymour" value="Seymour" />
-                            <label for="Seymour"><p>[<em>Placeholder for image of Seymour</em>]</p></label>
+                            <label htmlFor="Seymour"><p>[<em>Placeholder for image of Seymour</em>]</p></label>
                         </div>
                         <div>
                             <input type="checkbox" id="Nibbler" name="Nibbler" value="Nibbler" />
-                            <label for="Seymour"><p>[<em>Placeholder for image of Nibbler</em>]</p></label>
+                            <label htmlFor="Seymour"><p>[<em>Placeholder for image of Nibbler</em>]</p></label>
                         </div>
                     </fieldset>
                     <fieldset>
                         <legend>Where are you interested in meeting?</legend>
                         <div>
-                            <label for='address'>Address:</label>
+                            <label htmlFor='address'>Address:</label>
                             <input type="text" id="address" name="address" />
                         </div>
-                        <p>[<em>Placeholder for map widget</em>]</p>
+                        <Map />
                     </fieldset>
                     <fieldset>
                         <legend>When are you interested in meeting?</legend>
                         <fieldset class="sub-fieldset">
                             <legend>Looking for a one-time meeting on a specific date:</legend>
-                            <label for="once-date">Date:</label>
+                            <label htmlFor="once-date">Date:</label>
                             <input type="date" id="once-date" name="once-date" placeholder="yyyy-mm-dd" />
-                            <label for="once-start-time">I'm available from:</label>
+                            <label htmlFor="once-start-time">I'm available from:</label>
                             <input type="time" id="once-start-time" name="once-start-time" placeholder="10:30" />
-                            <label for="once-end-time">I'm available until:</label>
+                            <label htmlFor="once-end-time">I'm available until:</label>
                             <input type="time" id="once-end-time" name="once-end-time" placeholder="16:00" />
                             <button type="button">Add another time window on this date</button>
                         </fieldset>
                         <hr />
                         <fieldset class="sub-fieldset">
                             <legend>I'm open for dog dates during the following windows of time:</legend>
-                            <label for="window-day-1">Day of the week:</label>
+                            <label htmlFor="window-day-1">Day of the week:</label>
                             <select id="windodw-day-1">
                                 <option value="">--Select a day--</option>
                                 <option value="Monday">Monday</option>
@@ -58,14 +59,14 @@ const CreateHowl = (props) => {
                                 <option value="Saturday">Saturday</option>
                                 <option value="Sunday">Sunday</option>
                             </select>
-                            <label for="window-start-time-1">I'm available from:</label>
+                            <label htmlFor="window-start-time-1">I'm available from:</label>
                             <input type="time" id="window-start-time-1" name="window-start-time-1" placeholder="10:30" />
-                            <label for="window-end-time-1">I'm available until:</label>
+                            <label htmlFor="window-end-time-1">I'm available until:</label>
                             <input type="time" id="window-end-time-1" name="window-end-time-1" placeholder="16:00" />
                             <button type="button">Click to add another window</button>
                         </fieldset>
                     </fieldset>
-                    <label for="howl-description">Add a personal message to go with your howl:</label>
+                    <label htmlFor="howl-description">Add a personal message to go with your howl:</label>
                     <textarea id="howl-description" name="howl-description" maxlength="2000" placeholder="(Write your personal message here)" aria-describedby="char-limit"></textarea>
                     <p id="char-limit">(Limited to 2000 characters.)</p>
                     <button type="submit">Submit</button>
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,37 +1,63 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import config from '../config';
 import './Map.css';
 
 const Map = (props) => {
 
-    let mapContainer = useRef(null);
+    const [markerCoordinates, setMarkerCoordinates] = useState({});
+    const [map, setMap] = useState(null);
+    const [marker, setMarker] = useState(null);
 
-    const setRef = (el) => {
+    let mapContainer = useRef(null);
+    let markerRef = useRef(null);
+
+    const setRefMap = (el) => {
         mapContainer = el;
+    }
+
+    const setRefMark = (el) => {
+        markerRef = el;
     }
 
     useEffect(() => {
 
-        mapboxgl.accessToken = config.mapbox_key;
+        const locationiqKey = config.mapbox_key;
 
         const map = new mapboxgl.Map({
             container: mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            attributionControl: false,
+            style: `https://tiles.locationiq.com/v2/streets/vector.json?key=${locationiqKey}`,
             center: [-73.9665, 40.7812],
-            zoom: 5
+            zoom: 12
         });
 
-        const marker = new mapboxgl.Marker()
+        setMap(map);
+
+        const marker = new mapboxgl.Marker(markerRef, {
+            draggable: true,
+        })
             .setLngLat([-73.9665, 40.7812])
             .addTo(map);
+        
+        setMarker(marker);
+
+        const onDragEnd = () => {
+            const lngLat = marker.getLngLat();
+            setMarkerCoordinates(lngLat);
+        }
+
+        marker.on('dragend', onDragEnd);
 
     }, [props]);
 
+
     return (
         <div className='Map__map-outer-container'>
-            <div id='map-container' ref={el => setRef(el)} className='Map__map-container'>
+            <div id='map-container' ref={el => setRefMap(el)} className='Map__map-container'>
             </div>
+            <div className='Map__marker' ref={el => setRefMark(el)}></div>
+            <div id='coordinates' className='Map__coordinates'>{Object.keys(markerCoordinates).length ? `Latitude: ${markerCoordinates.lat}, Longitude: ${markerCoordinates.lng}`  : ''}</div>
         </div>
     );
 }
