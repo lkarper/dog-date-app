d053a8c9d9f5180bfcafed1e742efb77ef10bd66
Luther Karper <luther.karper@live.com>
2020-07-27 09:07:42 -0400

    Add nav links to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = (props) => {
     return (
         <header className="Header__header">
             <h1>
-                <Link to='/'>
+                <Link 
+                    to='/'
+                >
                     Dog Date
                 </Link>
             </h1>
             <nav>
-
+                <NavLink 
+                    className='Header__navlink' 
+                    to='/home'
+                >
+                    Homepage
+                </NavLink>
+                <NavLink 
+                    className='Header__navlink'  
+                    to='/howls'
+                >
+                    Howls
+                </NavLink>
+                <Link 
+                    className='Header__navlink' 
+                    to='/register'
+                >
+                    Register
+                </Link>
+                <Link 
+                    className='Header__navlink' 
+                    to='/login'
+                >
+                    Log in
+                </Link>
             </nav>
         </header>
     );
