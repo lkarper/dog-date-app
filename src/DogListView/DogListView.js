cb46c55f99433134018ea9afa6eadc9e3c5e5db6
Luther Karper <luther.karper@live.com>
2020-07-29 09:35:36 -0400

    Add link to dog profile  to DogListView component

--- src/DogListView/DogListView.js
+++ src/DogListView/DogListView.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './DogListView.css';
 
 const DogListView = (props) => {
 
-    const { profile_img_url, name } = props.dog;
+    const { profile_img_url, name, id } = props.dog;
 
     return (
         <li className='DogListView__li'>
@@ -13,7 +14,11 @@ const DogListView = (props) => {
                     src={profile_img_url} 
                     alt={`Profile picture for the dog named ${name}.`} 
                 />
-                <p>{name}</p>
+                <Link
+                    to={`/dog-profile/${id}`}
+                >
+                    {name}
+                </Link>
             </div>
         </li>
     );
