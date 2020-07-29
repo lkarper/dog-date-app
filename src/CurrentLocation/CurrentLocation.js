9f9fb6f786e7d0bb0f0d076b3f6adfa702e4476e
Luther Karper <luther.karper@live.com>
2020-07-29 17:25:07 -0400

    Add code to validate howl location
    Added code to validate the current location set in the CreateHowl form
    to the CurrentLocation component.

--- src/CurrentLocation/CurrentLocation.js
+++ src/CurrentLocation/CurrentLocation.js
@@ -2,15 +2,24 @@ import React, { useEffect } from 'react';
 
 const CurrentLocation = (props) => {
     
-    const { coordinates, address } = props;
+    const { coordinates, address, setLocationError } = props;
 
-    if (address) {
+    useEffect(() => {
+        if (address.trim().length) {
+            setLocationError(null);
+        } else {
+            setLocationError('Address or location name required.') 
+        }
+    }, [address, setLocationError])
+
+    if (address.trim().length) {
         return (
-            <p>Location set to: {address} ({Object.keys(coordinates).length ? `${coordinates.lat}, ${coordinates.lon}` : 'No coordinates set'})</p>
-        )
+            <p id='current-location'>Location set to: {address} ({Object.keys(coordinates).length ? `${coordinates.lat}, ${coordinates.lon}` : 'No coordinates set'})</p>
+        );
     }
-
-    return <p>Enter a location above to set a meeting place.</p>;
+    
+    return <p id='current-location'>Enter a location above to set a meeting place. An address or location name is required; coordinates are optional.</p>;
+    
 }
 
 export default CurrentLocation;
