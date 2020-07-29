c76aa51c5108fe451f3cccb91319babe70df9d8b
Luther Karper <luther.karper@live.com>
2020-07-28 20:18:58 -0400

    Update UserContext
    Added useEffect function so that a user's dogs are loaded into context
    when a user logs in.

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,5 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import STORE from '../STORE';
 
 const UserContext = createContext({
     setUser: () => {},
@@ -9,9 +10,18 @@ export default UserContext;
 export const UserProvider = (props) => {
 
     const [user, setUser] = useState({});
+    const [dogs, setDogs] = useState({});
+    
+    useEffect(() => {
+        if (Object.keys(user).length) {
+            const dogs = STORE.dog_profiles.filter(dog => dog.owner_id === user.id);
+            setDogs(dogs);
+        }
+    }, [user])
 
     const value = {
         user,
+        dogs,
         setUser,
     };
 
