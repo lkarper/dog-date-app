58b800d6b1548fd909981c1a9ecc643c82a6be99
Luther Karper <luther.karper@live.com>
2020-07-29 17:23:35 -0400

    Add OneTimeMeetingTimeWindow component

---
+++ src/OneTimeMeetingTimeWindow/OneTimeMeetingTimeWindow.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const OneTimeMeetingTimeWindow = (props) => {
+
+    const { index, initialData, updateTimeWindows, removeTimeWindow } = props;
+
+    const [startTime, setStartTime] = useState(initialData.startTime);
+    const [endTime, setEndTime] = useState(initialData.endTime);
+
+    useEffect(() => {
+        updateTimeWindows(index, startTime, endTime);
+    }, [startTime, endTime])
+
+    return (
+        <li>
+            <label htmlFor={`once-start-time-${index + 1}`}>I'm available from:</label>
+            <input 
+                type="time" 
+                id={`once-start-time-${index + 1}`} 
+                name={`once-start-time-${index + 1}`} 
+                placeholder="10:30"
+                value={startTime}
+                aria-describedby={`start-time-validator-${index + 1}`} 
+                onChange={(e) => setStartTime(e.target.value)}
+            />
+            <label htmlFor={`once-end-time-${index + 1}`}>I'm available until:</label>
+            <input 
+                type="time" 
+                id={`once-end-time-${index + 1}`} 
+                name={`once-end-time-${index + 1}`} 
+                min={startTime}
+                placeholder="16:00" 
+                value={endTime}
+                aria-describedby={`end-time-validator-${index + 1}`}
+                onChange={(e) => setEndTime(e.target.value)}
+            />
+            {index !== 0 ? <button type='button' onClick={() => removeTimeWindow(index)}>Remove window</button> : ''}
+        </li>   
+    );
+}
+
+export default OneTimeMeetingTimeWindow;
