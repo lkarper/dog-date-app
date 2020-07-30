22d7185b30a538963f4f380623278f7bf1c48887
Luther Karper <luther.karper@live.com>
2020-07-30 14:19:04 -0400

    Update ValidateEmail component
    Component updated to display message if email is already linked to a
    registered account.

--- src/validation-components/registration-validation/ValidateEmail.js
+++ src/validation-components/registration-validation/ValidateEmail.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import isEmail from 'validator/lib/isEmail';
 
 const ValidateEmail = (props) => {
-    const { emailValidationError } = props;
+    const { emailValidationError, emailAlreadyRegistered } = props;
 
     useEffect(() => {
         const { email, setEmailValidationError } = props;
@@ -14,10 +15,18 @@ const ValidateEmail = (props) => {
     }, [props]);
 
     if (emailValidationError) {
-        return <p id='email-validation'>A valid email address is required.</p>;
+        return (
+            <p id='email-validation'>A valid email address is required.
+                {emailAlreadyRegistered ? `A registered account is already linked to the email address '${emailAlreadyRegistered}'.  If that account belongs to you, ${<Link to='/login'>click here</Link>} to login.` : ''}    
+            </p>
+        );
     }
 
-    return <p id='email-validation'>Email accepted.</p>
+    return (
+        <p id='email-validation'>Email properly formatted.
+            {emailAlreadyRegistered ? `A registered account is already linked to the email address '${emailAlreadyRegistered}'.  If that account belongs to you, ${<Link to='/login'>click here</Link>} to login.` : ''}    
+        </p>
+    );
    
 }
 
