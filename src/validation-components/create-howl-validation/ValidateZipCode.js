5bd9efd2bcdab882ece0026e6cd8c49c740a0dcb
Luther Karper <luther.karper@live.com>
2020-07-30 14:16:47 -0400

    Add ValidateZipCode component

---
+++ src/validation-components/create-howl-validation/ValidateZipCode.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+const ValidateZipCode = (props) => {
+    
+    const { zipcode, zipcodeError, setZipcodeError } = props;
+
+    useEffect(() => {
+        
+        if (!/(^[0-9]{5}$)|(^[0-9]{5}-[0-9]{4}$)/.test(zipcode)) {
+            setZipcodeError(`ZIP code is required an must match format '12345' or '12345-6789'.`);
+        } else {
+            setZipcodeError('');
+        }
+
+    }, [zipcode, setZipcodeError]);
+
+    if (zipcodeError) {
+        return <p id='zipcode-validator'>{zipcodeError}</p>;
+    }
+
+    return <p id='zipcode-validator'>ZIP code set to {zipcode}.</p>;
+}
+
+export default ValidateZipCode;
