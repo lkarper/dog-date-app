326d3857b8d55ca7f69f674997b03b3cdf156534
Luther Karper <luther.karper@live.com>
2020-07-31 14:41:03 -0400

    Add error handling if dog to be reviewed not found
    If the dog that a user is attempting to review cannot be found by the
    id supplied in the route, then a message is displayed and a list of the
    user's pack members is displayed with links to review each dog.

--- src/DogReviewPage/DogReviewPage.js
+++ src/DogReviewPage/DogReviewPage.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
 import DogReviewForm from '../DogReviewForm/DogReviewForm';
+import DogListView from '../DogListView/DogListView';
 
 const DogReviewPage = (props) => {
+
     const { dog_id } = props.match.params;
+
+    const context = useContext(UserContext);
+
+    const dog = context.allDogs.find(dog => dog.id === dog_id);
 
     const [friendlinessDogsP, setFriendlinessDogsP] = useState('');
     const [friendlinessPeopleP, setFriendlinessPeopleP] = useState('');
@@ -24,12 +32,45 @@ const DogReviewPage = (props) => {
         endTime: '',
     });
 
+    if (!dog) {
+        const pack = context.packMembers.map(pm => 
+            context.allDogs.find(dog => 
+                dog.id === pm.pack_member_id)
+            );
+
+        return (
+            <section>
+                <header>
+                    <h2>Dog not found</h2>
+                </header>
+                <p>We couldn't find the dog that you're attempting to review.</p>
+                {pack.length 
+                ?             
+                    <div>
+                        <h3>Would you like to review one of your pack members?</h3>
+                        <ul>
+                            {pack.map(dog => 
+                                <div key={dog.id}>
+                                    <DogListView dog={dog} />
+                                    <Link to={`/leave-review/${dog.id}`}>Click here to review {dog.name}</Link>
+                                </div>
+                            )}
+                        </ul>
+                    </div>
+                :
+                    <Link>Search for dogs</Link>
+            }
+            </section>
+        );
+    }
+
     return (
         <section>
             <header>
-                <h2>Use this form to leave a review of Seymour</h2>
+                <h2>Use this form to leave a review of {dog.name}</h2>
             </header>
             <DogReviewForm
+                dogName={dog.name}
                 friendlinessDogsP={friendlinessDogsP}
                 setFriendlinessDogsP={setFriendlinessDogsP}
                 friendlinessPeopleP={friendlinessPeopleP}
@@ -52,7 +93,7 @@ const DogReviewPage = (props) => {
                 setWhenP={setWhenP}
             />
         </section>
-    )
+    );
 }
 
 export default DogReviewPage;
