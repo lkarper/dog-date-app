8c95eb8deb4a47bc88abf63ba81244d7cb1ce28f
Luther Karper <luther.karper@live.com>
2020-08-03 13:42:12 -0400

    Add ValidateDogProfileSex component

--- src/validation-components/create-dog-profile-validation/ValidateDogProfileSex.js
+++ src/validation-components/create-dog-profile-validation/ValidateDogProfileSex.js
@@ -13,16 +13,10 @@ const ValidateDogProfileSex = (props) => {
     }, [sex, setSexError]);
 
     if (sex) {
-        const sexDisplay = sex
-            .split('-')
-            .join(', ');
-        const newSexDisplay = `${sexDisplay.charAt(0).toUpperCase()}${sexDisplay.slice(1)}`;
-
-        return <p id='sex-validator'>Sex set to: {newSexDisplay}.</p>;
+        return <p id='sex-validator'>Sex set to: '{sex}.'</p>;
     }
 
     return <p id='sex-validator'>{sexError}</p>;
-
 }
 
 export default ValidateDogProfileSex;
