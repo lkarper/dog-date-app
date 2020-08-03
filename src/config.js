b409521111778928505b821b1c8379fbf301161e
Luther Karper <luther.karper@live.com>
2020-08-03 13:44:52 -0400

    Add environmental variables

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
     mapbox_key: process.env.REACT_APP_MAPBOX_TOKEN,
+    cloud_name: process.env.REACT_APP_CLOUDINARY_NAME,
+    preset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
 }
