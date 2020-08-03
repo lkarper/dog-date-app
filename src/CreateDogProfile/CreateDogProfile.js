7a5b7ab89e20daebf9796588424b7b3ef4beb418
Luther Karper <luther.karper@live.com>
2020-08-03 13:45:49 -0400

    Decompose CreateDogProfile component
    Decomposed CreateDogProfile component into smaller components.

--- src/CreateDogProfile/CreateDogProfile.js
+++ src/CreateDogProfile/CreateDogProfile.js
@@ -1,134 +1,209 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import UserContext from '../contexts/UserContext';
 import BasicInfo from '../CreateDogProfileFormComponents/BasicInfo/BasicInfo';
+import DogDescription from '../CreateDogProfileFormComponents/DogDescription/DogDescription';
+import UploadDogProfilePhoto from '../CreateDogProfileFormComponents/UploadDogProfilePhoto/UploadDogProfilePhoto';
 
 const CreateDogProfile = (props) => {
 
-    return (
-        <form className='profile-creation-form'>
-           <BasicInfo />
-            <fieldset>
-                <legend>Describe your dog:</legend>
-                <fieldset className="sub-fieldset">
-                    <legend>How energetic is your dog?</legend>
-                    <div>
-                        <input type="radio" id="low-energy" name="energy-level" value='low-energy' />
-                        <label htmlFor="low-energy">Not very...</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="medium-energy" name="energy-level" value='medium-energy' />
-                        <label htmlFor="medium-energy">About as energetic as the average dog.</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="high-energy" name="energy-level" value='high-energy' />
-                        <label htmlFor="high-energy">Very energetic.</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="extreme-energy" name="energy-level" value='extreme-energy' />
-                        <label htmlFor="extreme-energy">So...much...energy...it never runs out.</label>
-                    </div>
-                </fieldset>
-                <hr />
-                <fieldset className="sub-fieldset">
-                    <legend>How would you describe your dog's temperment?</legend>
-                    <div>
-                        <input type="radio" id="temperment-nervous" name="temperment" value="nervous" />
-                        <label htmlFor="temperment-nervous">Nervous around new people or dogs</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="temperment-shy" name="temperment" value="shy" />
-                        <label htmlFor="temperment-shy">Shy, but not skiddish or nervous</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="temperment-indifferent" name="temperment" value="indifferent" />
-                        <label htmlFor="temperment-indifferent">Somewhat indifferent to most dogs</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="temperment-outgoing" name="temperment" value="outgoing" />
-                        <label htmlFor="temperment-outgoing">Outgoing and eager to be friends with most dogs</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="temperment-aggressive" name="temperment" value="aggressive" />
-                        <label htmlFor="temperment-aggressive">On the aggressive side and always trying to be the alpha</label>
-                    </div>
-                </fieldset>
-                <hr />
-                <fieldset className="sub-fieldset">
-                    <legend>How well trained and obedient is your dog?</legend>
-                    <div>
-                        <input type="radio" id="no-training" name="training-level" value="no training" />
-                        <label htmlFor="no-training">Not trained at all and struggles with obedience...</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="some-training" name="training-level" value="some training" />
-                        <label htmlFor="some-training">Some training, but not always obedient</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="well-trained" name="training-level" value="well-trained" />
-                        <label htmlFor="well-trained">Well trained in basic commands and generally obedient</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="very-well-trained" name="training-level" value="very-well-trained" />
-                        <label htmlFor="very-well-trained">Very-well trained and always obedient</label>
-                    </div>
-                </fieldset>
-                <hr />
-                <fieldset className="sub-fieldset">
-                    <legend>What else should others know about your dog?</legend>
-                    <div>
-                        <input type="checkbox" id="other-dislikes-puppies" name="other-dislikes-puppies" value="dislikes puppies" />
-                        <label htmlFor="other-dislikes-puppies">Dislikes puppies</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-dislikes-men" name="other-dislikes-men" value="dislikes men" />
-                        <label htmlFor="other-dislikes-men">Dislikes men</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-dislikes-women" name="other-dislikes-women" value="dislikes women" />
-                        <label htmlFor="other-dislikes-women">Dislikes women</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-no-children" name="other-no-children" value="not good with children" />
-                        <label htmlFor="other-no-children">Not good with children</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-recently-adopted" name="other-recently-adopted" value="recently adopted" />
-                        <label htmlFor="other-recently-adopted">Recently adopted</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-loves-people" name="other-loves-people" value="prefers people to dogs" />
-                        <label htmlFor="other-loves-people">Prefers people to dogs</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-leash-aggression" name="other-leash-aggression" value="has leash aggression" />
-                        <label htmlFor="other-leash-aggression">Has leash aggression</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-elderly" name="other-elderly" value="elderly dog" />
-                        <label htmlFor="other-elderly">Elderly dog</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-little-experience" name="other-little-experience" value="has spent little time with other dogs" />
-                        <label htmlFor="other-little-experience">Has spent little time with other dogs</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-much-experience" name="other-much-experience" value="has a lot of experience playing with other dogs" />
-                        <label htmlFor="other-much-experience">Has a lot of experience playing with other dogs</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="other-food-aggression" name="other-food-aggression" value="aggressive around and protective of food" />
-                        <label htmlFor="other-food-aggression">Aggressive around and protective of food</label>
-                    </div>
-                </fieldset>
-                <hr />
-                <div>
-                    <label htmlFor="own-description">Describe your dog in your own words:</label>
-                    <textarea id="own-description" name="own-description" maxLength="2000" placeholder="My dog is..." aria-describedby="char-limit"></textarea>
-                    <p id="char-limit">(Limited to 2000 characters.)</p>
-                </div>
-            </fieldset>
-            <button type="submit">Submit</button>
-        </form>
-    );
+    const context = useContext(UserContext);
+
+    const [infoForm, setInfoForm] = useState(true);
+    const [imgUploadForm, setImgUploadForm] = useState(false);
+    const [nameP, setNameP] = useState('');
+    const [nameErrorP, setNameErrorP] = useState('');
+    const [ageYearsP, setAgeYearsP] = useState('');
+    const [ageMonthsP, setAgeMonthsP] = useState('');
+    const [ageErrorP, setAgeErrorP] = useState('');
+    const [sexP, setSexP] = useState('');
+    const [sexErrorP, setSexErrorP] = useState('');
+    const [breedP, setBreedP] = useState('');
+    const [weightP, setWeightP] = useState('');
+    const [energyP, setEnergyP] = useState('');
+    const [energyErrorP, setEnergyErrorP] = useState('');
+    const [tempermentP, setTempermentP] = useState('');
+    const [tempermentErrorP, setTempermentErrorP] = useState('');
+    const [obedienceP, setObedienceP] = useState('');
+    const [obedienceErrorP, setObedienceErrorP] = useState('');
+    const [dislikesPuppiesP, setDislikesPuppiesP] = useState('');
+    const [dislikesMenP, setDislikesMenP] = useState('');
+    const [dislikesWomenP, setDislikesWomenP] = useState('');
+    const [noChildrenP, setNoChildrenP] = useState('');
+    const [recentlyAdoptedP, setRecentlyAdoptedP] = useState('');
+    const [lovesPeopleP, setLovesPeopleP] = useState('');
+    const [leashAggressionP, setLeashAggressionP] = useState('');
+    const [elderlyDogP, setElderlyDogP] = useState('');
+    const [littleExperienceP, setLittleExperienceP] = useState('');
+    const [muchExperienceP, setMuchExperienceP] = useState('');
+    const [foodAggressionP, setFoodAggressionP] = useState('');
+    const [personalMessageP, setPersonalMessageP] = useState('');
+    const [personalMessageErrorP, setPersonalMessageErrorP] = useState('');
+    const [imgUrlP, setImgUrlP] = useState('');
+
+    const handleInfoSubmit = (event) => {
+        event.preventDefault();
+        setInfoForm(false);
+    }
+
+    const uploadDogProfile = () => {
+        const newDogProfile = {
+            id: uuidv4(),
+            owner_id: context.user.id,
+            name: nameP,
+            profile_img_url: imgUrlP,
+            age_years: ageYearsP,
+            age_months: ageMonthsP,
+            sex: sexP,
+            breed: breedP,
+            weight: weightP,
+            energy: energyP,
+            temperment: tempermentP,
+            obedience: obedienceP,
+            dislikes_puppies: !!dislikesPuppiesP,
+            dislikes_men: !!dislikesMenP,
+            dislikes_women: !!dislikesWomenP,
+            dislikes_children: !!noChildrenP,
+            recently_adopted: !!recentlyAdoptedP,
+            prefers_people: !!lovesPeopleP,
+            leash_aggression: !!leashAggressionP,
+            elderly_dog: !!elderlyDogP,
+            little_time_with_other_dogs: !!littleExperienceP,
+            much_experience_with_other_dogs: !!muchExperienceP,
+            aggressive: !!foodAggressionP,
+            owner_description: personalMessageP,
+        };
+
+        context.addDogProfile(newDogProfile);
+        props.history.push(`/dog-profile/${newDogProfile.id}`);
+    }
+
+    if (infoForm) {
+        return (
+            <>
+                <header>
+                    <h1>Create a Profile</h1>
+                </header>
+                <section>
+                    <header>
+                        <h2>Start barking about your dog!</h2>
+                    </header>
+                    <form 
+                        onSubmit={handleInfoSubmit}
+                        className='profile-creation-form'
+                    >
+                        <BasicInfo 
+                            data={{
+                                nameP,
+                                setNameP,
+                                nameErrorP,
+                                setNameErrorP,
+                                ageYearsP,
+                                setAgeYearsP,
+                                ageMonthsP,
+                                setAgeMonthsP,
+                                ageErrorP,
+                                setAgeErrorP,
+                                sexP,
+                                setSexP,
+                                sexErrorP,
+                                setSexErrorP,
+                                breedP,
+                                setBreedP,
+                                weightP,
+                                setWeightP,
+                            }}
+                        />
+                        <DogDescription 
+                            data={{
+                                energyP,
+                                setEnergyP,
+                                energyErrorP,
+                                setEnergyErrorP,
+                                tempermentP,
+                                setTempermentP,
+                                tempermentErrorP,
+                                setTempermentErrorP,
+                                obedienceP,
+                                setObedienceP,
+                                obedienceErrorP,
+                                setObedienceErrorP,
+                                dislikesPuppiesP,
+                                setDislikesPuppiesP,
+                                dislikesMenP,
+                                setDislikesMenP,
+                                dislikesWomenP,
+                                setDislikesWomenP,
+                                noChildrenP,
+                                setNoChildrenP,
+                                recentlyAdoptedP,
+                                setRecentlyAdoptedP,
+                                lovesPeopleP,
+                                setLovesPeopleP,
+                                leashAggressionP,
+                                setLeashAggressionP,
+                                elderlyDogP,
+                                setElderlyDogP,
+                                littleExperienceP,
+                                setLittleExperienceP,
+                                muchExperienceP,
+                                setMuchExperienceP,
+                                foodAggressionP,
+                                setFoodAggressionP,
+                                personalMessageP,
+                                setPersonalMessageP,
+                                personalMessageErrorP,
+                                setPersonalMessageErrorP,
+                            }}
+                        />
+                        <button 
+                            type="submit"
+                            disabled={
+                                nameErrorP ||
+                                ageErrorP ||
+                                sexErrorP ||
+                                energyErrorP ||
+                                tempermentErrorP ||
+                                obedienceErrorP ||
+                                personalMessageErrorP
+                            }
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </section>
+            </>
+        );
+    } else {
+        if (!imgUploadForm) {
+            return (
+                <>
+                    <header>
+                        <h1>Create a Profile</h1>
+                    </header>
+                    <section>
+                        <header>
+                            <h2>Would you like to upload a photo for your dog's profile?</h2>
+                        </header>
+                        <button onClick={() => setImgUploadForm(true)}>Yes</button>
+                        <button onClick={uploadDogProfile}>No</button>
+                    </section>
+                </>
+            )
+        }
+        return (
+            <>
+                <header>
+                    <h1>Create a Profile</h1>
+                </header>
+                <UploadDogProfilePhoto 
+                    imgUrlP={imgUrlP}
+                    setImgUrlP={setImgUrlP}
+                    uploadDogProfile={uploadDogProfile}
+                />
+            </>
+        );
+    }
 }
 
 export default CreateDogProfile;
