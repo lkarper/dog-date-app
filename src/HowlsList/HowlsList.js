d0455cf1fa959043d460859ccc6b77014a4c0652
Luther Karper <luther.karper@live.com>
2020-08-03 21:24:45 -0400

    Update HowlsList component
    Made HowlsList a controlled component and decomposed the form.

--- src/HowlsList/HowlsList.js
+++ src/HowlsList/HowlsList.js
@@ -3,10 +3,51 @@ import UserContext from '../contexts/UserContext';
 import { Link } from 'react-router-dom';
 import HowlListItem from '../HowlListItem/HowlListItem';
 
+import { calculateAverageWithArrayOfReviews } from '../DogAverageRating/DogAverageRating';
+import HowlsPageFilterForm from '../HowlsPageFilterForm/HowlsPageFilterForm';
+
 const HowlsList = (props) => {
 
     const context = useContext(UserContext);
-    const { howls } = context;
+
+    const [howls, setHowls] = useState(context.howls);
+    const [stateP, setStateP] = useState('');
+    const [zipcodeP, setZipcodeP] = useState('');
+    const [ratingFilterP, setRatingFilterP] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let filteredHowls = [...context.howls];
+        if (stateP) {
+            filteredHowls = filteredHowls.filter(howl => howl.location.state === stateP);
+        }
+        if (zipcodeP) {
+            filteredHowls = filteredHowls.filter(howl => howl.location.zipcode === zipcodeP);
+        }
+        if (ratingFilterP) {
+            const arrayOfPassingDogIds = filteredHowls
+                .map(howl => howl.dog_ids)
+                .flat()
+                .map(dog_id => 
+                    context.reviews
+                        .filter(review => review.dog_id === dog_id)
+                )
+                .filter(arrayOfReviews => 
+                    calculateAverageWithArrayOfReviews(arrayOfReviews) >= parseInt(ratingFilterP)
+                )
+                .map(arrayOfReviews => arrayOfReviews[0].dog_id);
+            filteredHowls = filteredHowls.filter(howl => {
+                let includeHowl = false;
+                arrayOfPassingDogIds.forEach(passing_id => {
+                    if (howl.dog_ids.includes(passing_id)) {
+                        includeHowl = true;
+                    }
+                })
+                return includeHowl;
+            });
+        } 
+        setHowls(filteredHowls);
+    }
 
     return (
         <>
@@ -18,57 +59,31 @@ const HowlsList = (props) => {
                     Start howling yourself!
                 </Link>
             </header> 
-            <form>
-                <fieldset>
-                    <legend>Filter howls by:</legend>
-                    <fieldset class="sub-fieldset">
-                        <legend>Distance</legend>
-                        <div>
-                            <input type="radio" id="sort-howls-distance-5" name="sort-howls-distance" value="5" checked />
-                            <label for="sort-howls-distance-5">Less than 5 miles away</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-distance-15" name="sort-howls-distance" value="15" />
-                            <label for="sort-howls-distance-15">Less than 15 miles away</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-distance-25" name="sort-howls-distance" value="25" />
-                            <label for="sort-howls-distance-25">Less than 25 miles away</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-distance-any" name="sort-howls-distance" value="any" />
-                            <label for="sort-howls-distance-any">Show all howls regardless of location</label>
-                        </div>
-                    </fieldset>
-                    <fieldset class="sub-fieldset">
-                        <legend>Average rating of dog(s)</legend>
-                        <div>
-                            <input type="radio" id="sort-howls-rating-4" name="sort-howls-rating" value="4" />
-                            <label for="sort-howls-rating-4">4+</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-rating-3" name="sort-howls-rating" value="3" />
-                            <label for="sort-howls-rating-3">3+</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-rating-2" name="sort-howls-rating" value="2" />
-                            <label for="sort-howls-rating-2">2+</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="sort-howls-rating-any" name="sort-howls-rating" value="any" checked />
-                            <label for="sort-howls-rating-any">Show all howls regardless of rating</label>
-                        </div>
-                    </fieldset>
-                </fieldset>
-                <a href="">Advanced Search</a>
-            </form>
+            <HowlsPageFilterForm 
+                data={{
+                    stateP,
+                    setStateP,
+                    zipcodeP,
+                    setZipcodeP,
+                    ratingFilterP,
+                    setRatingFilterP,
+                    handleSubmit,
+                }}
+            />
             <section>
                 <header>
                     <h2>Dogs howling for a playmate</h2>
                 </header>
-                <ol>
-                    {howls.map(howl => <HowlListItem key={howl.id} howl={howl} />)}
-                </ol>
+                <div role='alert'>
+                    {howls.length === 0 
+                        ?
+                            <p>No howls found that match those search criteria.  Adust your parameters and try again.</p>
+                        :
+                            <ol>
+                                {howls.map(howl => <HowlListItem key={howl.id} howl={howl} />)}
+                            </ol>
+                    }
+                </div>
             </section>
         </>
     );
