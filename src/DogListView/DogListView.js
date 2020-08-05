54867e6c604d732e59aa2f36efe06c1e4b743701
Luther Karper <luther.karper@live.com>
2020-08-05 14:23:23 -0400

    Add classNames

--- src/DogListView/DogListView.js
+++ src/DogListView/DogListView.js
@@ -8,13 +8,14 @@ const DogListView = (props) => {
 
     return (
         <li className='DogListView__li'>
-            <div>
+            <div className='DogListView__dog-container'>
                 <img
                     className='DogListView__profile-pic' 
                     src={profile_img_url} 
                     alt={`Avatar of the dog named ${name}.`} 
                 />
                 <Link
+                    className='DogListView__profile-link'
                     to={`/dog-profile/${id}`}
                 >
                     View {name}'s profile
