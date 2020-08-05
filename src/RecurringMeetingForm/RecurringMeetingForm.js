73965c92beb8bfd20c369e52e81e0b173c3426a5
Luther Karper <luther.karper@live.com>
2020-08-05 14:46:11 -0400

    Update RecurringMeetingForm component
    Wrapped label+input in a div and added classNames.

--- src/RecurringMeetingForm/RecurringMeetingForm.js
+++ src/RecurringMeetingForm/RecurringMeetingForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './RecurringMeetingForm.css';
 
 const RecurringMeetingForm = (props) => {
 
@@ -29,46 +30,52 @@ const RecurringMeetingForm = (props) => {
         <li>
             <fieldset className="sub-fieldset">
                 <legend>I'm open for dog dates during the following windows of time:</legend>
-                <label htmlFor={`window-day-${index + 1}`}>Day of the week:</label>
-                <select 
-                    id={`window-day-${index + 1}`}
-                    value={dayOfWeek}
-                    onChange={(e) => setDayOfWeek(e.target.value)}
-                    aria-describedby={`day-validator-${index + 1}`}
-                    required
-                >
-                    <option value="">--Select a day--</option>
-                    <option value="Monday">Monday</option>
-                    <option value="Tuesday">Tuesday</option>
-                    <option value="Wednesday">Wednesday</option>
-                    <option value="Thursday">Thursday</option>
-                    <option value="Friday">Friday</option>
-                    <option value="Saturday">Saturday</option>
-                    <option value="Sunday">Sunday</option>
-                </select>
-                <label htmlFor={`window-start-time-${index + 1}`} >I'm available from:</label>
-                <input 
-                    type="time" 
-                    id={`window-start-time-${index + 1}`} 
-                    name={`window-start-time-${index + 1}`} 
-                    placeholder="10:30" 
-                    value={startTime}
-                    aria-describedby={`start-time-validator-${index + 1}`}
-                    onChange={(e) => setStartTime(e.target.value)}
-                    required
-                />
-                <label htmlFor={`window-end-time-${index + 1}`}>I'm available until:</label>
-                <input 
-                    type="time" 
-                    id={`window-end-time-${index + 1}`} 
-                    name={`window-end-time-${index + 1}`}
-                    placeholder="16:00"
-                    min={startTime}
-                    value={endTime}
-                    aria-describedby={`end-time-validator-${index + 1}`}
-                    onChange={(e) => setEndTime(e.target.value)} 
-                    required
-                />
+                <div className='RecurringMeetingForm__input-container'>
+                    <label htmlFor={`window-day-${index + 1}`}>Day of the week:</label>
+                    <select 
+                        id={`window-day-${index + 1}`}
+                        value={dayOfWeek}
+                        onChange={(e) => setDayOfWeek(e.target.value)}
+                        aria-describedby={`day-validator-${index + 1}`}
+                        required
+                    >
+                        <option value="">--Select a day--</option>
+                        <option value="Monday">Monday</option>
+                        <option value="Tuesday">Tuesday</option>
+                        <option value="Wednesday">Wednesday</option>
+                        <option value="Thursday">Thursday</option>
+                        <option value="Friday">Friday</option>
+                        <option value="Saturday">Saturday</option>
+                        <option value="Sunday">Sunday</option>
+                    </select>
+                </div>
+                <div className='RecurringMeetingForm__input-container'>
+                    <label htmlFor={`window-start-time-${index + 1}`} >I'm available from:</label>
+                    <input 
+                        type="time" 
+                        id={`window-start-time-${index + 1}`} 
+                        name={`window-start-time-${index + 1}`} 
+                        placeholder="10:30" 
+                        value={startTime}
+                        aria-describedby={`start-time-validator-${index + 1}`}
+                        onChange={(e) => setStartTime(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className='RecurringMeetingForm__input-container'>
+                    <label htmlFor={`window-end-time-${index + 1}`}>I'm available until:</label>
+                    <input 
+                        type="time" 
+                        id={`window-end-time-${index + 1}`} 
+                        name={`window-end-time-${index + 1}`}
+                        placeholder="16:00"
+                        min={startTime}
+                        value={endTime}
+                        aria-describedby={`end-time-validator-${index + 1}`}
+                        onChange={(e) => setEndTime(e.target.value)} 
+                        required
+                    />
+                </div>
                 {index !== 0 
                     ? <button type='button' onClick={() => removeRecurringMeetingWindow(index)}>Remove this window</button>
                     : ''
