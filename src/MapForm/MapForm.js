b2e7b60fa049c7af727d8c17d7d86bbf6310a5cf
Luther Karper <luther.karper@live.com>
2020-08-05 14:41:29 -0400

    Add classNames

--- src/MapForm/MapForm.js
+++ src/MapForm/MapForm.js
@@ -9,14 +9,20 @@ const MapForm = (props) => {
     const [tempCoordinates, setTempCoordinates] = useState({ lat: 40.7812, lon: -73.9665 });
 
     return (
-        <fieldset>
+        <fieldset className='MapForm__coordinates-fieldset'>
             <legend>Set coordinates for your meeting here</legend>
             <p>Using this feature will make it easier for others to find the location you set, but it is not required.</p>
             <Map 
                 tempCoordinates={tempCoordinates} 
                 setTempCoordinates={setTempCoordinates} 
             />
-            <button type='button' onClick={() => setCoordinates(tempCoordinates)}>Set meeting place to above coordinates</button>
+            <button 
+                className='MapForm__set-coordinates-button'
+                type='button' 
+                onClick={() => setCoordinates(tempCoordinates)}
+            >
+                Set meeting place to above coordinates
+            </button>
             <CurrentLocation 
                 coordinates={coordinates}
             />
