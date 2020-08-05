24720fe9590737347e65bb517fe715f590ac9b05
Luther Karper <luther.karper@live.com>
2020-08-05 14:46:55 -0400

    Update ReviewFormStarRater component
    Wrapped label+input in a div and updated classNames.

--- src/ReviewFormStarRater/ReviewFormStarRater.js
+++ src/ReviewFormStarRater/ReviewFormStarRater.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './ReviewFormStarRater.css';
 
 const ReviewFormStarRater = (props) => {
 
@@ -15,60 +16,70 @@ const ReviewFormStarRater = (props) => {
     return (
         <fieldset className="sub-fieldset">
             <legend>{legendText}</legend>
-            <div className="rate">
-                <input 
-                    type="radio" 
-                    id={`star5-${classSuffix}`} 
-                    name={`rate-${classSuffix}`} 
-                    value="5" 
-                    checked={starRating === '5'}
-                    onChange={(e) => setStarRating(e.target.value)}
-                    required
-                />
-                <label htmlFor={`star5-${classSuffix}`}>5 stars</label>
-                <input 
-                    type="radio" 
-                    id={`star4-${classSuffix}`} 
-                    name={`rate-${classSuffix}`} 
-                    value="4" 
-                    checked={starRating === '4'}
-                    onChange={(e) => setStarRating(e.target.value)}
-                    required
-                />
-                <label htmlFor={`star4-${classSuffix}`}>4 stars</label>
-                <input 
-                    type="radio" 
-                    id={`star3-${classSuffix}`} 
-                    name={`rate-${classSuffix}`} 
-                    value="3" 
-                    checked={starRating === '3'}
-                    onChange={(e) => setStarRating(e.target.value)}
-                    required
-                />
-                <label htmlFor={`star3-${classSuffix}`}>3 stars</label>
-                <input 
-                    type="radio" 
-                    id={`star2-${classSuffix}`} 
-                    name={`rate-${classSuffix}`} 
-                    value="2" 
-                    checked={starRating === '2'}
-                    onChange={(e) => setStarRating(e.target.value)}
-                    required
-                />
-                <label htmlFor={`star2-${classSuffix}`}>2 stars</label>
-                <input 
-                    type="radio" 
-                    id={`star1-${classSuffix}`} 
-                    name={`rate-${classSuffix}`} 
-                    value="1" 
-                    checked={starRating === '1'}
-                    onChange={(e) => setStarRating(e.target.value)}
-                    required
-                />
-                <label htmlFor={`star1-${classSuffix}`}>1 stars</label>  
+            <div className="ReviewFormStarRater__rate-container">
+                <div>
+                    <input 
+                        type="radio" 
+                        id={`star5-${classSuffix}`} 
+                        name={`rate-${classSuffix}`} 
+                        value="5" 
+                        checked={starRating === '5'}
+                        onChange={(e) => setStarRating(e.target.value)}
+                        required
+                    />
+                    <label htmlFor={`star5-${classSuffix}`}>5 stars</label>
+                </div>
+                <div>
+                    <input 
+                        type="radio" 
+                        id={`star4-${classSuffix}`} 
+                        name={`rate-${classSuffix}`} 
+                        value="4" 
+                        checked={starRating === '4'}
+                        onChange={(e) => setStarRating(e.target.value)}
+                        required
+                    />
+                    <label htmlFor={`star4-${classSuffix}`}>4 stars</label>
+                </div>
+                <div>
+                    <input 
+                        type="radio" 
+                        id={`star3-${classSuffix}`} 
+                        name={`rate-${classSuffix}`} 
+                        value="3" 
+                        checked={starRating === '3'}
+                        onChange={(e) => setStarRating(e.target.value)}
+                        required
+                    />
+                    <label htmlFor={`star3-${classSuffix}`}>3 stars</label>
+                </div>
+                <div>
+                    <input 
+                        type="radio" 
+                        id={`star2-${classSuffix}`} 
+                        name={`rate-${classSuffix}`} 
+                        value="2" 
+                        checked={starRating === '2'}
+                        onChange={(e) => setStarRating(e.target.value)}
+                        required
+                    />
+                    <label htmlFor={`star2-${classSuffix}`}>2 stars</label>
+                </div>
+                <div>
+                    <input 
+                        type="radio" 
+                        id={`star1-${classSuffix}`} 
+                        name={`rate-${classSuffix}`} 
+                        value="1" 
+                        checked={starRating === '1'}
+                        onChange={(e) => setStarRating(e.target.value)}
+                        required
+                    />
+                    <label htmlFor={`star1-${classSuffix}`}>1 stars</label>  
+                </div>
             </div>
         </fieldset>
-    )
+    );
 }
 
 export default ReviewFormStarRater;
