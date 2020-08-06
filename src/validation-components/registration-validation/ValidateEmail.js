85052eee6ba04005570b6c925ee4cb7c6edcc355
Luther Karper <luther.karper@live.com>
2020-08-06 14:40:33 -0400

    Fix bug in ValidateEmail component
    Fixed a bug that was causing an object to be interpolated in a string
    literal by converting the string literal to JSX.

--- src/validation-components/registration-validation/ValidateEmail.js
+++ src/validation-components/registration-validation/ValidateEmail.js
@@ -14,17 +14,21 @@ const ValidateEmail = (props) => {
         }
     }, [props]);
 
+    const accountExistsText = (<>A registered account is already linked to the email address '{emailAlreadyRegistered}'.  If that account belongs to you, <Link to='/login'>click here</Link> to login.</>);
+
     if (emailValidationError) {
         return (
             <p id='email-validation'>A valid email address is required.
-                {emailAlreadyRegistered ? `A registered account is already linked to the email address '${emailAlreadyRegistered}'.  If that account belongs to you, ${<Link to='/login'>click here</Link>} to login.` : ''}    
+                <br />
+                {emailAlreadyRegistered && accountExistsText}    
             </p>
         );
     }
 
     return (
         <p id='email-validation'>Email properly formatted.
-            {emailAlreadyRegistered ? `A registered account is already linked to the email address '${emailAlreadyRegistered}'.  If that account belongs to you, ${<Link to='/login'>click here</Link>} to login.` : ''}    
+            <br />
+            {emailAlreadyRegistered && accountExistsText}    
         </p>
     );
    
