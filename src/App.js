b6d138bd9dfd95b62fb325349fb72a8a7783edca
Luther Karper <luther.karper@live.com>
2020-08-08 11:14:52 -0400

    Add ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -12,64 +12,66 @@ import CreateDogReviewPage from './CreateDogReviewPage/CreateDogReviewPage';
 import CreateDogProfile from './CreateDogProfile/CreateDogProfile';
 import HowlPageView from './HowlPageView/HowlPageView';
 import ReviewPage from './ReviewPage/ReviewPage';
-import './App.css';
 import PageNotFound from './PageNotFound/PageNotFound';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <main>
-        <Switch>
-          <Route
-            exact path='/'
-            component={Landing}
-          />
-          <Route 
-            path='/login'
-            component={LoginPage}
-          />
-          <Route 
-            path='/register'
-            component={RegistrationForm}
-          />
-          <Route 
-            exact path='/howls'
-            component={HowlsList}
-          />
-          <Route 
-            path='/howls/:howl_id'
-            component={HowlPageView}
-          />
-          <Route 
-            path='/home'
-            component={Homepage}
-          />
-          <Route 
-            path='/create-howl'
-            component={CreateHowlPage}
-          />
-          <Route
-            path='/dog-profile/:id'
-            component={DogProfilePage} 
-          />
-          <Route 
-            path='/reviews/:id'
-            component={ReviewPage}
-          />
-          <Route 
-            path='/leave-review/:dog_id'
-            component={CreateDogReviewPage}
-          />
-          <Route 
-            path='/create-dog-profile'
-            component={CreateDogProfile}
-          />
-          <Route 
-            component={PageNotFound}
-          />
-        </Switch>
+        <ErrorBoundary>
+          <Switch>
+            <Route
+              exact path='/'
+              component={Landing}
+            />
+            <Route 
+              path='/login'
+              component={LoginPage}
+            />
+            <Route 
+              path='/register'
+              component={RegistrationForm}
+            />
+            <Route 
+              exact path='/howls'
+              component={HowlsList}
+            />
+            <Route 
+              path='/howls/:howl_id'
+              component={HowlPageView}
+            />
+            <Route 
+              path='/home'
+              component={Homepage}
+            />
+            <Route 
+              path='/create-howl'
+              component={CreateHowlPage}
+            />
+            <Route
+              path='/dog-profile/:id'
+              component={DogProfilePage} 
+            />
+            <Route 
+              path='/reviews/:id'
+              component={ReviewPage}
+            />
+            <Route 
+              path='/leave-review/:dog_id'
+              component={CreateDogReviewPage}
+            />
+            <Route 
+              path='/create-dog-profile'
+              component={CreateDogProfile}
+            />
+            <Route 
+              component={PageNotFound}
+            />
+          </Switch>
+        </ErrorBoundary>
       </main>
     </div>
   );
