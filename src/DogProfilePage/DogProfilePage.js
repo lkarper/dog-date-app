9ffcb1700b27711c6e6b10687d33c685a1038894
Luther Karper <luther.karper@live.com>
2020-08-08 10:38:27 -0400

    Add error handling to DogProfilePage
    Added error handling to DogProfilePage component when profile not
    found by id.

--- src/DogProfilePage/DogProfilePage.js
+++ src/DogProfilePage/DogProfilePage.js
@@ -18,70 +18,82 @@ const DogProfilePage = (props) => {
 
     const { id } = props.match.params;
     const dog_profile = context.allDogs.find(profile => profile.id === id);
-    const howlsBy = context.howls.filter(howl => howl.dog_ids.includes(id));
-    const reviews = context.reviews.filter(review => review.dog_id === id);
+
+    if (dog_profile) {
+        const howlsBy = context.howls.filter(howl => howl.dog_ids.includes(id));
+        const reviews = context.reviews.filter(review => review.dog_id === id);
+
+        return (
+            <>
+                <header className='DogProfilePage__header'>
+                    <h1>{dog_profile.name}</h1>
+                    <img 
+                        className='DogProfilePage__profile-img'
+                        src={dog_profile.profile_img_url}
+                        alt={`Avatar for the dog named ${dog_profile.name}.`} 
+                    />
+                    <DogProfilePageHeaderButtons 
+                        dog_profile={dog_profile}
+                    />
+                </header>
+                <section className='DogProfilePage__about-section'>
+                    <header className='DogProfilePage__header'>
+                        <h2>About {dog_profile.name}</h2>
+                    </header>
+                    <p>{dog_profile.owner_description}</p>
+                    <p>Age: {dog_profile.age_months ? `${dog_profile.age_years} years, ${dog_profile.age_months}, months` : `${dog_profile.age_years}`}</p>
+                    <p>Breed: {dog_profile.breed || `(not listed)`}</p>
+                    <p>Weight: {dog_profile.weight ? `${dog_profile.weight} lbs` : `(Not listed)`}</p>
+                    <p>Sex: {dog_profile.sex || `(not listed)`}</p>
+                </section>
+                <section>
+                    <header className='DogProfilePage__header'>
+                        <h2>Characteristics</h2>
+                    </header>
+                <DogProfileCharacteristics dog_profile={dog_profile}/>
+                </section>
+                <section>
+                    <header className='DogProfilePage__header'>
+                        <h2>Howls about {dog_profile.name}</h2>
+                    </header>
+                    <ul className='DogProfilePage__howls-list'>
+                        {howlsBy.map(howl => <HowlListItem key={howl.id} howl={howl}/>)}
+                    </ul>
+                </section>
+                <section>
+                    <header className='DogProfilePage__header'>
+                        <h2>Reviews of {dog_profile.name}</h2>
+                    </header>
+                    {context.user.id === dog_profile.owner_id 
+                        ? '' 
+                        : <Link to={`/leave-review/${dog_profile.id}`}>Leave your own review of {dog_profile.name}</Link>
+                    }
+                    {reviews.length 
+                        ? 
+                            <div>
+                                <DogAverageRating 
+                                    reviews={reviews}
+                                />
+                                <ul className='DogProfilePage__reviews-list'>
+                                    {reviews.map(review => <DogReviewListItem key={review.id} review={review} />)}
+                                </ul>
+                            </div>
+                        :
+                            <p>No reviews of {dog_profile.name} yet.</p>
+                }
+                </section>
+            </>
+        );
+    }
 
     return (
-        <>
-            <header className='DogProfilePage__header'>
-                <h1>{dog_profile.name}</h1>
-                <img 
-                    className='DogProfilePage__profile-img'
-                    src={dog_profile.profile_img_url}
-                    alt={`Avatar for the dog named ${dog_profile.name}.`} 
-                />
-                <DogProfilePageHeaderButtons 
-                    dog_profile={dog_profile}
-                />
+        <section className='section'>
+            <header>
+                <h1>Woof...</h1>
             </header>
-            <section className='DogProfilePage__about-section'>
-                <header className='DogProfilePage__header'>
-                    <h2>About {dog_profile.name}</h2>
-                </header>
-                <p>{dog_profile.owner_description}</p>
-                <p>Age: {dog_profile.age_months ? `${dog_profile.age_years} years, ${dog_profile.age_months}, months` : `${dog_profile.age_years}`}</p>
-                <p>Breed: {dog_profile.breed || `(not listed)`}</p>
-                <p>Weight: {dog_profile.weight ? `${dog_profile.weight} lbs` : `(Not listed)`}</p>
-                <p>Sex: {dog_profile.sex || `(not listed)`}</p>
-            </section>
-            <section>
-                <header className='DogProfilePage__header'>
-                    <h2>Characteristics</h2>
-                </header>
-               <DogProfileCharacteristics dog_profile={dog_profile}/>
-            </section>
-            <section>
-                <header className='DogProfilePage__header'>
-                    <h2>Howls about {dog_profile.name}</h2>
-                </header>
-                <ul className='DogProfilePage__howls-list'>
-                    {howlsBy.map(howl => <HowlListItem key={howl.id} howl={howl}/>)}
-                </ul>
-            </section>
-            <section>
-                <header className='DogProfilePage__header'>
-                    <h2>Reviews of {dog_profile.name}</h2>
-                </header>
-                {context.user.id === dog_profile.owner_id 
-                    ? '' 
-                    : <Link to={`/leave-review/${dog_profile.id}`}>Leave your own review of {dog_profile.name}</Link>
-                }
-                {reviews.length 
-                    ? 
-                        <div>
-                            <DogAverageRating 
-                                reviews={reviews}
-                            />
-                            <ul className='DogProfilePage__reviews-list'>
-                                {reviews.map(review => <DogReviewListItem key={review.id} review={review} />)}
-                            </ul>
-                        </div>
-                    :
-                        <p>No reviews of {dog_profile.name} yet.</p>
-            }
-            </section>
-        </>
-    )
+            <p>Dog profile not found.  Check the url and try agian.</p>
+        </section>
+    );
 }
 
 export default DogProfilePage;
