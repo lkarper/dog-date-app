bcb01a97802aec4512715b822ab877d04082c0c5
Luther Karper <luther.karper@live.com>
2020-08-08 10:26:09 -0400

    Add PageNotFound component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header/Header';
 import Landing from './Landing/Landing';
 import RegistrationForm from './RegistrationForm/RegistrationForm';
@@ -13,6 +13,7 @@ import CreateDogProfile from './CreateDogProfile/CreateDogProfile';
 import HowlPageView from './HowlPageView/HowlPageView';
 import ReviewPage from './ReviewPage/ReviewPage';
 import './App.css';
+import PageNotFound from './PageNotFound/PageNotFound';
 
 
 const App = () => {
@@ -20,50 +21,55 @@ const App = () => {
     <div className="App">
       <Header />
       <main>
-        <Route
-          exact path='/'
-          component={Landing}
-        />
-        <Route 
-          path='/login'
-          component={LoginPage}
-        />
-        <Route 
-          path='/register'
-          component={RegistrationForm}
-        />
-        <Route 
-          exact path='/howls'
-          component={HowlsList}
-        />
-        <Route 
-          path='/howls/:howl_id'
-          component={HowlPageView}
-        />
-        <Route 
-          path='/home'
-          component={Homepage}
-        />
-        <Route 
-          path='/create-howl'
-          component={CreateHowlPage}
-        />
-        <Route
-          path='/dog-profile/:id'
-          component={DogProfilePage} 
-        />
-        <Route 
-          path='/reviews/:id'
-          component={ReviewPage}
-        />
-        <Route 
-          path='/leave-review/:dog_id'
-          component={CreateDogReviewPage}
-        />
-        <Route 
-          path='/create-dog-profile'
-          component={CreateDogProfile}
-        />
+        <Switch>
+          <Route
+            exact path='/'
+            component={Landing}
+          />
+          <Route 
+            path='/login'
+            component={LoginPage}
+          />
+          <Route 
+            path='/register'
+            component={RegistrationForm}
+          />
+          <Route 
+            exact path='/howls'
+            component={HowlsList}
+          />
+          <Route 
+            path='/howls/:howl_id'
+            component={HowlPageView}
+          />
+          <Route 
+            path='/home'
+            component={Homepage}
+          />
+          <Route 
+            path='/create-howl'
+            component={CreateHowlPage}
+          />
+          <Route
+            path='/dog-profile/:id'
+            component={DogProfilePage} 
+          />
+          <Route 
+            path='/reviews/:id'
+            component={ReviewPage}
+          />
+          <Route 
+            path='/leave-review/:dog_id'
+            component={CreateDogReviewPage}
+          />
+          <Route 
+            path='/create-dog-profile'
+            component={CreateDogProfile}
+          />
+          <Route 
+            component={PageNotFound}
+          />
+        </Switch>
       </main>
     </div>
   );
---
+++ src/PageNotFound/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PageNotFound = (props) => {
+    return (
+        <section className='section'>
+            <header>
+                <h1>Woof...</h1>
+            </header>
+            <p>We couldn't sniff out the page you're looking for.  Check the address and try again.</p>
+        </section>
+    );
+}
+
+export default PageNotFound;
