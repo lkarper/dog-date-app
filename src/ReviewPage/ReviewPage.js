0e3b5e3b31b80066359a0e4cacd07b947724755a
Luther Karper <luther.karper@live.com>
2020-08-08 10:35:19 -0400

    Add error handling to ReviewPage when id not found

--- src/ReviewPage/ReviewPage.js
+++ src/ReviewPage/ReviewPage.js
@@ -24,30 +24,6 @@ const ReviewPage = (props) => {
     const { id } = props.match.params;
     const review = context.reviews.find(review => review.id === id);
     
-    const { 
-        reviewer,
-        date_created,
-        friendliness_dogs,
-        friendliness_people,
-        playing_interest,
-        obedience,
-        profile_accuracy,
-        location_suitability,
-        location,
-        when,
-        personal_message,
-        dog_id
-    } = review;
-
-    const averageRating = (
-        friendliness_dogs +
-        friendliness_people +
-        playing_interest +
-        obedience +
-        profile_accuracy + 
-        location_suitability 
-    ) / 6;
-
     let dog_profile;
     let reviews;
     let userButtons = <></>;
@@ -61,6 +37,31 @@ const ReviewPage = (props) => {
     }
 
     if (review) {
+
+        const { 
+            reviewer,
+            date_created,
+            friendliness_dogs,
+            friendliness_people,
+            playing_interest,
+            obedience,
+            profile_accuracy,
+            location_suitability,
+            location,
+            when,
+            personal_message,
+            dog_id
+        } = review;
+    
+        const averageRating = (
+            friendliness_dogs +
+            friendliness_people +
+            playing_interest +
+            obedience +
+            profile_accuracy + 
+            location_suitability 
+        ) / 6;
+
         dog_profile = context.allDogs.find(dog => dog.id === dog_id);
         reviews = context.reviews.filter(review => review.dog_id === dog_profile.id);
         if (Object.keys(context.user)) {
@@ -73,107 +74,114 @@ const ReviewPage = (props) => {
                 );
             }
         }
+
+        return (
+            <>
+                <header className='ReviewPage__header'>
+                    <h1>Review of {dog_profile.name}</h1>
+                    <p>Reviewed by: {reviewer}</p>
+                    <p>On: {moment(date_created).format("MMMM Do YYYY, h:mm a")}</p>
+                    <p>Overall rating: {averageRating.toFixed(2)} stars</p>
+                    {userButtons}
+                </header>
+                <section className='ReviewPage__section section'>
+                    {showEdit 
+                        ?
+                            <section className='ReviewPage__edit-container'>
+                                <header>
+                                    <h2>Edit your review</h2>
+                                </header>
+                                <button 
+                                    className='ReviewPage__edit-cancel-button'
+                                    onClick={() => setShowEdit(false)}
+                                >
+                                    &#10006;
+                                </button>
+                                <DogReviewForm 
+                                    dogName={dog_profile.name} 
+                                    dog_id={dog_profile.id} 
+                                    suffix='-edit' 
+                                    review={review} 
+                                    setShowEdit={setShowEdit} 
+                                />
+                            </section> 
+                        : ''
+                    }
+                    <header>
+                        <h2>About Seymour</h2>
+                    </header>
+                        <ul>
+                            <li>Friendliness towards other dogs: {friendliness_dogs}</li>
+                            <li>Friendliness towards people: {friendliness_people}</li>
+                            <li>Interest in playing: {playing_interest}</li>
+                            <li>Obedience: {obedience}</li>
+                            <li>Accuracy of profile: {profile_accuracy}</li>
+                        </ul>
+                        <h3>This is what {reviewer} had to say about {dog_profile.name}</h3>
+                        <blockquote>{personal_message}</blockquote>
+                </section>
+                <section className='ReviewPage__section section'>
+                    <header>
+                        <h2>About the playdate:</h2>
+                    </header>
+                    <h3>Location</h3>
+                    <p>Location suitabilty for a playdate: {location_suitability}</p>
+                    <p>Location: {location.address}, {location.city}, {location.state}{' '}{location.zipcode}</p>
+                    {location.lat && location.lon 
+                        ?
+                            <StaticMap 
+                                lat={location.lat}
+                                lon={location.lon}
+                            />
+                        : <p>Sorry, no map available.</p>
+                    }
+                    <h3>Date and time:</h3> 
+                    <p>{moment(when.date).format("dddd, MMMM Do, YYYY")}</p>
+                    {<TimeWindow startTime={when.startTime} endTime={when.endTime} />}
+                </section>
+                <section className='ReviewPage__section section'>
+                    <header>
+                        <h2>Comments</h2>
+                    </header>
+                    <ReviewComments reviewId={id}/>
+                    {Object.keys(context.user).length && <AddCommentForm reviewId={id}/>}
+                </section>
+                <section className='ReviewPage__section section'>
+                    <header>
+                        <h2>Other reviews of {dog_profile.name}</h2>
+                    </header>
+                    {context.user.id === dog_profile.owner_id 
+                        ? '' 
+                        : <Link to={`/leave-review/${dog_profile.id}`}>Leave your own review of {dog_profile.name}</Link>
+                    }
+                    {reviews.length 
+                        ? 
+                            <div>
+                                <DogAverageRating 
+                                    reviews={reviews}
+                                />
+                                <ul className='ReviewPage__reviews-list'>
+                                    {reviews.map(review => <DogReviewListItem key={review.id} review={review} />)}
+                                </ul>
+                            </div>
+                        :
+                            <p>No reviews of {dog_profile.name} yet.</p>
+                }
+                </section>
+            </>
+        );
     }
     
-    if (!review) {
-        return <p>Review not found.  Check the url and try again.</p>
-    }
-    
+
     return (
-        <>
-            <header className='ReviewPage__header'>
-                <h1>Review of {dog_profile.name}</h1>
-                <p>Reviewed by: {reviewer}</p>
-                <p>On: {moment(date_created).format("MMMM Do YYYY, h:mm a")}</p>
-                <p>Overall rating: {averageRating.toFixed(2)} stars</p>
-                {userButtons}
+        <section className='section'>
+            <header>
+                <h1>Woof...</h1>
             </header>
-            <section className='ReviewPage__section section'>
-                {showEdit 
-                    ?
-                        <section className='ReviewPage__edit-container'>
-                            <header>
-                                <h2>Edit your review</h2>
-                            </header>
-                            <button 
-                                className='ReviewPage__edit-cancel-button'
-                                onClick={() => setShowEdit(false)}
-                            >
-                                &#10006;
-                            </button>
-                            <DogReviewForm 
-                                dogName={dog_profile.name} 
-                                dog_id={dog_profile.id} 
-                                suffix='-edit' 
-                                review={review} 
-                                setShowEdit={setShowEdit} 
-                            />
-                        </section> 
-                    : ''
-                }
-                <header>
-                    <h2>About Seymour</h2>
-                </header>
-                    <ul>
-                        <li>Friendliness towards other dogs: {friendliness_dogs}</li>
-                        <li>Friendliness towards people: {friendliness_people}</li>
-                        <li>Interest in playing: {playing_interest}</li>
-                        <li>Obedience: {obedience}</li>
-                        <li>Accuracy of profile: {profile_accuracy}</li>
-                    </ul>
-                    <h3>This is what {reviewer} had to say about {dog_profile.name}</h3>
-                    <blockquote>{personal_message}</blockquote>
-            </section>
-            <section className='ReviewPage__section section'>
-                <header>
-                    <h2>About the playdate:</h2>
-                </header>
-                <h3>Location</h3>
-                <p>Location suitabilty for a playdate: {location_suitability}</p>
-                <p>Location: {location.address}, {location.city}, {location.state}{' '}{location.zipcode}</p>
-                {location.lat && location.lon 
-                    ?
-                        <StaticMap 
-                            lat={location.lat}
-                            lon={location.lon}
-                        />
-                    : <p>Sorry, no map available.</p>
-                }
-                <h3>Date and time:</h3> 
-                <p>{moment(when.date).format("dddd, MMMM Do, YYYY")}</p>
-                {<TimeWindow startTime={when.startTime} endTime={when.endTime} />}
-            </section>
-            <section className='ReviewPage__section section'>
-                <header>
-                    <h2>Comments</h2>
-                </header>
-                <ReviewComments reviewId={id}/>
-                {Object.keys(context.user).length && <AddCommentForm reviewId={id}/>}
-            </section>
-            <section className='ReviewPage__section section'>
-                <header>
-                    <h2>Other reviews of {dog_profile.name}</h2>
-                </header>
-                {context.user.id === dog_profile.owner_id 
-                    ? '' 
-                    : <Link to={`/leave-review/${dog_profile.id}`}>Leave your own review of {dog_profile.name}</Link>
-                }
-                {reviews.length 
-                    ? 
-                        <div>
-                            <DogAverageRating 
-                                reviews={reviews}
-                            />
-                            <ul className='ReviewPage__reviews-list'>
-                                {reviews.map(review => <DogReviewListItem key={review.id} review={review} />)}
-                            </ul>
-                        </div>
-                    :
-                        <p>No reviews of {dog_profile.name} yet.</p>
-            }
-            </section>
-        </>
+            <p>Review not found.  Check the url and try again.</p>
+        </section>
     );
+
 }
 
 export default ReviewPage;
