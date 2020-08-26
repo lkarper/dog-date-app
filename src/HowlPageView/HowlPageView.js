e0edcbd8062fa6caf64c8a44853ca65ade91f795
Luther Karper <luther.karper@live.com>
2020-08-26 17:24:27 -0400

    Update HowlPageView component to use api

--- src/HowlPageView/HowlPageView.js
+++ src/HowlPageView/HowlPageView.js
@@ -8,26 +8,32 @@ import TimeWindow from '../TimeWindow/TimeWindow';
 import './HowlPageView.css';
 import HowlPageUserButtons from '../HowlPageUserButtons/HowlPageUserButtons';
 import CreateHowl from '../CreateHowl/CreateHowl';
+import HowlsService from '../services/howls-service';
 
 const HowlPageView  = (props) => {
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [props]);
-
     const context = useContext(UserContext);
+
+    const { user } = context;
 
     const { howl_id } = props.match.params;
 
     const [showEdit, setShowEdit] = useState(false);
+    const [howl, setHowl] = useState();
+    const [howlNotFound, setHowlNotFound] = useState(false);
 
-    const howl = context.howls.find(howl => howl.id === howl_id);
-
-    let user;
-
-    if (howl) {
-        user = STORE.users.find(user => user.id === howl.user_id);
-    }
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        HowlsService.getHowlById(howl_id)
+            .then(howl => {
+                setHowl(howl);
+            })
+            .catch(error => {
+                if (error.message === `Howl doesn't exist`) {
+                    setHowlNotFound(true);
+                }
+            })
+    }, [props, howl_id, setHowl, setHowlNotFound]);
 
     let howlDateTime;
     
@@ -35,15 +41,15 @@ const HowlPageView  = (props) => {
         if (howl.meeting_type === 'once') {
             howlDateTime = (            
                 <div className='HowlPageView__time-list'>
-                    <p>Date: {moment(howl.one_time_windows.date).format("dddd, MMMM Do, YYYY")}</p>
+                    <p>Date: {moment(howl.date).format("dddd, MMMM Do, YYYY")}</p>
                     <ul>
-                        {howl.one_time_windows.timeWindows
-                            .sort((a,b) => parseInt(a.startTime.split(':')[0]) - parseInt(b.startTime.split(':')[0]))
+                        {howl.time_windows
+                            .sort((a, b) => parseInt(a.start_time.split(':')[0]) - parseInt(b.start_time.split(':')[0]))
                             .map((window, i) => (
                                 <li key={i}>
                                     <TimeWindow 
-                                        startTime={window.startTime}
-                                        endTime={window.endTime}
+                                        startTime={window.start_time}
+                                        endTime={window.end_time}
                                     />
                                 </li>
                             ))}
@@ -54,12 +60,12 @@ const HowlPageView  = (props) => {
             howlDateTime = (
                 <div className='HowlPageView__time-list'>
                     <ul>
-                        {howl.recurring_windows.map((window, i) => (
+                        {howl.time_windows.map((window, i) => (
                             <li key={i}>
-                                <p>{window.dayOfWeek}</p>
+                                <p>{window.day_of_week}</p>
                                 <TimeWindow
-                                    startTime={window.startTime}
-                                    endTime={window.endTime}
+                                    startTime={window.start_time}
+                                    endTime={window.end_time}
                                 />
                             </li>
                         ))}
@@ -69,7 +75,7 @@ const HowlPageView  = (props) => {
         }
     }
 
-    if (!howl) {
+    if (howlNotFound) {
         return (
             <>
                 <header>
@@ -81,68 +87,73 @@ const HowlPageView  = (props) => {
         );
     }
 
-    return (
-        <>
-            <header className='HowlPageView__header'>
-                <h1>{howl.howl_title}</h1>
-                <HowlPageUserButtons howl={howl} setShowEdit={setShowEdit} />
-            </header>
-            {showEdit && <CreateHowl howl={howl} suffix='-edit' setShowEdit={setShowEdit} />}
-            <section className='HowlPageView__section section'>
-                <header>
-                    <h2>About this howl:</h2>
+    if (howl) {
+        return (
+            <>
+                <header className='HowlPageView__header'>
+                    <h1>{howl.howl_title}</h1>
+                    <HowlPageUserButtons howl={howl} setShowEdit={setShowEdit} />
                 </header>
-                <p>{howl.personal_message}</p>
-            </section>
-            <section className='HowlPageView__section section'>
-                <header>
-                    <h2>Availability</h2>
-                </header>
-                {howlDateTime}
-            </section>
-            <section className='HowlPageView__section section'>
-                <header>
-                    <h2>Location</h2>
-                </header>
-                <div className='HowlPageView__address'>
-                    <p>{howl.location.address}</p>
-                    <p>{howl.location.city}</p>
-                    <p>{howl.location.state}{', '}{howl.location.zipcode}</p>
-                </div>
-                {howl.location.lat && howl.location.lon 
-                    ? 
-                        <StaticMap 
-                            lat={howl.location.lat}
-                            lon={howl.location.lon}
-                        />
-                    : <p>Map not available for this location.</p>
-                }
-            </section>
-            <section className='HowlPageView__section section'>
-                <header>
-                    <h2>Contact info for {user.username}</h2>
-                </header>
-                <p>Phone: {user.phone || '(Not given.)'}</p>
-                <p>Email: {user.email}</p>
-            </section>
-            <section className='HowlPageView__section section'>
-                <header>
-                    <h2>Dogs in this howl</h2>
-                </header>
-                <ul className='HowlPageView__dog-list'>
-                    {howl.dog_ids
-                        .map(id => context.allDogs.find(dog => dog.id === id))
-                        .map(dog_profile => 
-                            <HowlPageDogProfile 
-                                key={dog_profile.id} 
-                                dog_profile={dog_profile} 
+                {showEdit && <CreateHowl howl={howl} suffix='-edit' setShowEdit={setShowEdit} />}
+                <section className='HowlPageView__section section'>
+                    <header>
+                        <h2>About this howl:</h2>
+                    </header>
+                    <p>{howl.personal_message}</p>
+                </section>
+                <section className='HowlPageView__section section'>
+                    <header>
+                        <h2>Availability</h2>
+                    </header>
+                    {howlDateTime}
+                </section>
+                <section className='HowlPageView__section section'>
+                    <header>
+                        <h2>Location</h2>
+                    </header>
+                    <div className='HowlPageView__address'>
+                        <p>{howl.location.address}</p>
+                        <p>{howl.location.city}</p>
+                        <p>{howl.location.state}{', '}{howl.location.zipcode}</p>
+                    </div>
+                    {howl.location.lat && howl.location.lon 
+                        ? 
+                            <StaticMap 
+                                lat={howl.location.lat}
+                                lon={howl.location.lon}
                             />
-                        )
+                        : <p>Map not available for this location.</p>
                     }
-                </ul>
-            </section>
-        </>
-    );
+                </section>
+                <section className='HowlPageView__section section'>
+                    <header>
+                        <h2>Contact info for {user.username}</h2>
+                    </header>
+                    <p>Phone: {user.phone || '(Not given.)'}</p>
+                    <p>Email: {user.email}</p>
+                </section>
+                <section className='HowlPageView__section section'>
+                    <header>
+                        <h2>Dogs in this howl</h2>
+                    </header>
+                    <ul className='HowlPageView__dog-list'>
+                        {howl.dogs
+                            .map(dog =>
+                                <HowlPageDogProfile 
+                                    key={dog.dog_id}
+                                    dog_id={dog.dog_id} 
+                                    dog_profile={dog.profile}
+                                    owner={dog.owner} 
+                                />
+                            )
+                        }
+                    </ul>
+                </section>
+            </>
+        );
+    }
+
+    return <p>Loading...</p>;
 }
 
 export default HowlPageView;
