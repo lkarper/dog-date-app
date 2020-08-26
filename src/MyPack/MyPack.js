bbf551f5d48906283440889e772ed1154f1c1481
Luther Karper <luther.karper@live.com>
2020-08-26 17:22:28 -0400

    Update MyPack component to use api

--- src/MyPack/MyPack.js
+++ src/MyPack/MyPack.js
@@ -7,20 +7,16 @@ const MyPack = (props) => {
 
     const context = useContext(UserContext);
     const { packMembers } = context;
-    const pack = packMembers.map(pm => 
-        context.allDogs.find(dog => 
-            dog.id === pm.pack_member_id)
-        );
 
     return (
         <section className='MyPack__section section'>
             <header>
                 <h2>My Pack</h2>
             </header>
-            {pack.length 
+            {packMembers.length 
                 ?             
                     <ul className='MyPack__list'>
-                        {pack.map(dog => <DogListView key={dog.id} dog={dog} />)}
+                        {packMembers.map(dog => <DogListView key={dog.id} dog={dog.profile} />)}
                     </ul>
                 :
                     <p>No pack members yet.</p>
