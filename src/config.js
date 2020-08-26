c77b979567925a46c084393ae46733312cd19b3c
Luther Karper <luther.karper@live.com>
2020-08-26 11:48:51 -0400

    Add api endpoint to env vars

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     mapbox_key: process.env.REACT_APP_MAPBOX_TOKEN,
-    cloud_name: process.env.REACT_APP_CLOUDINARY_NAME,
-    preset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    TOKEN_KEY: 'dog-date-auth-token',
 }
