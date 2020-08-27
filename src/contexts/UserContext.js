006289cc6c00125cc62f321951c304fa75b8b22a
Luther Karper <luther.karper@live.com>
2020-08-27 16:03:35 -0400

    Remove unused code from UserContext
    Now that the api is fully integrated, removed unused code in context.

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,21 +1,10 @@
-import React, { useState, useEffect, createContext } from 'react';
-import STORE from '../STORE';
+import React, { useState, createContext } from 'react';
 
 const UserContext = createContext({
     user: {},
     dogs: [],
-    allDogs: [],
-    howls: [],
     userSavedHowls: [],
     packMembers: [],
-    reviews: [],
-    reviewComments: [],
-    addReview: () => {},
-    removeReview: () => {},
-    updateReview: () => {},
-    addHowl: () => {},
-    removeHowl: () => {},
-    updateHowl: () => {},
     addPackMember: () => {},
     removePackMember: () => {},
     setUser: () => {},
@@ -24,9 +13,6 @@ const UserContext = createContext({
     updateDogProfile: () => {},
     addSavedHowl: () => {},
     removeSavedHowl: () => {},
-    addComment: () => {},
-    updateComment: () => {},
-    removeComment: () => {},
     setDogs: () => {},
     setUserSavedHowls: () => {},
     setUserPackMembers: () => {},
@@ -38,53 +24,8 @@ export const UserProvider = (props) => {
 
     const [dogs, setDogs] = useState([]);
     const [user, setUser] = useState({});
-    const [allDogs, setAllDogs] = useState(STORE.dog_profiles);
-    const [howls, setHowls] = useState(STORE.howls);
-    const [reviews, setReviews] = useState(STORE.reviews);
     const [userSavedHowls, setUserSavedHowls] = useState([]);
     const [packMembers, setUserPackMembers] = useState([]);
-    const [reviewComments, setReviewComments] = useState(STORE.review_comments);
-    
-    useEffect(() => {
-        if (Object.keys(user).length) {
-            const dogs = STORE.dog_profiles.filter(dog => dog.owner_id === user.id);
-            setDogs(dogs);
-            const savedHowls = STORE.user_saved_howls.filter(howl => howl.user_id === user.id);
-            setUserSavedHowls(savedHowls);
-            const userPackMembers = STORE.pack_members.filter(pm => pm.user_id === user.id);
-            setUserPackMembers(userPackMembers);
-        }
-    }, [user]);
-
-    const addHowl = (newHowl) => {
-        const updatedHowls = [...howls, newHowl];
-        setHowls(updatedHowls);
-    }
-
-    const removeHowl = (howlId) => {
-        const updatedHowls = howls.filter(howl => howl.id !== howlId);
-        setHowls(updatedHowls);
-    }
-
-    const updateHowl = (newHowl) => {
-        const updatedHowls = [ ...howls.filter(howl => howl.id !== newHowl.id), newHowl];
-        setHowls(updatedHowls);
-    }
-
-    const addReview = (newReview) => {
-        const updatedReviews = [ ...reviews, newReview];
-        setReviews(updatedReviews);
-    }
-
-    const updateReview = (newReview) => {
-        const updatedReviews = [...reviews.filter(r => r.id !== newReview.id), newReview];
-        setReviews(updatedReviews);
-    }
-
-    const removeReview = (idToRemove) => {
-        const updatedReviews = reviews.filter(r => r.id !== idToRemove);
-        setReviews(updatedReviews);
-    }
 
     const addPackMember = (newPackMember) => {
         const updatedPackMembers = [...packMembers, newPackMember];
@@ -97,8 +38,8 @@ export const UserProvider = (props) => {
     }
 
     const addDogProfile = (newDogProfile) => {
-        const updateDogs = [...dogs, newDogProfile];
-        setDogs(updateDogs);
+        const updatedDogs = [...dogs, newDogProfile];
+        setDogs(updatedDogs);
     }
 
     const removeDogProfile = (profileId) => {
@@ -110,9 +51,6 @@ export const UserProvider = (props) => {
         const updatedDogs = dogs.filter(dog => dog.id !== newDogProfile.id);
         updatedDogs.push(newDogProfile);
         setDogs(updatedDogs);
-        const updatedAllDogs = allDogs.filter(dog => dog.id !== newDogProfile.id);
-        updatedAllDogs.push(newDogProfile);
-        setAllDogs(updatedAllDogs);
     }
 
     const addSavedHowl = (newHowl) => {
@@ -125,33 +63,12 @@ export const UserProvider = (props) => {
         setUserSavedHowls(updatedHowls);
     }
 
-    const addComment = (newComment) => {
-        const updatedComments = [...reviewComments, newComment];
-        setReviewComments(updatedComments);
-    }
-
-    const updateComment = (updatedComment) => {
-        const updatedComments = [...reviewComments.filter(com => com.id !== updatedComment.id), updatedComment];
-        setReviewComments(updatedComments);
-    }
-
-    const removeComment = (idToRemove) => {
-        const updatedComments = [...reviewComments.filter(com => com.id !== idToRemove)];
-        setReviewComments(updatedComments);
-    }
-
     const value = {
         user,
         dogs,
-        allDogs,
-        howls,
         userSavedHowls,
         packMembers,
-        reviews,
-        reviewComments,
-        addReview,
         setUser,
-        addHowl,
         addPackMember,
         removePackMember,
         addDogProfile,
@@ -159,13 +76,6 @@ export const UserProvider = (props) => {
         updateDogProfile,
         addSavedHowl,
         removeSavedHowl,
-        addComment,
-        removeHowl,
-        updateHowl,
-        updateReview,
-        removeReview,
-        updateComment,
-        removeComment,
         setDogs,
         setUserSavedHowls,
         setUserPackMembers,
