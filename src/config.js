78970c388a7a22da3593200a87ad0ac8cf37d7a5
Luther Karper <luther.karper@live.com>
2020-08-28 11:02:13 -0400

    Add dev api endpoint to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     mapbox_key: process.env.REACT_APP_MAPBOX_TOKEN,
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: 'dog-date-auth-token',
 }
