de4c3423795daeef4aadc56b37db7cec3edb8618
Luther Karper <luther.karper@live.com>
2020-08-28 12:44:46 -0400

    Refactor code

--- src/AddCommentForm/AddCommentForm.js
+++ src/AddCommentForm/AddCommentForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import UserContext from '../contexts/UserContext';
-import './AddCommentForm.css';
 import ReviewsService from '../services/reviews-service';
+import './AddCommentForm.css';
 
 const AddCommentForm = (props) => {
 
