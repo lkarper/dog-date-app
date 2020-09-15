b1da6cd13bf1b70a4c602e0d83d408c87a00aa11
Luther Karper <luther.karper@live.com>
2020-09-15 13:42:03 -0400

    Add tests for CreateDogProfile

---
+++ src/CreateDogProfile/CreateDogProfile.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import CreateDogProfile from './CreateDogProfile';
+
+global.scrollTo = jest.fn();
+
+describe('CreateDogProfile component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <BrowserRouter>
+                <CreateDogProfile />
+            </BrowserRouter>,
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = mount(
+            <BrowserRouter>
+                <CreateDogProfile />
+            </BrowserRouter>
+        );
+        expect(toJSON(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
+    });
+});
