b49aa80a5ac767392e996a045c3b2eeb82649220
Luther Karper <luther.karper@live.com>
2020-09-15 19:37:18 -0400

    Add tests to DogListView component

---
+++ src/DogListView/DogListView.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json';
+import DogListView from './DogListView';
+
+describe('DogListView component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<DogListView />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = mount(<DogListView />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
+    });
+});
