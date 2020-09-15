670805bac11fe00d241104aa020ca6158364adf2
Luther Karper <luther.karper@live.com>
2020-09-15 14:21:13 -0400

    Update setupTests.js
    Add global mock implementations.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.URL.createObjectURL = jest.fn();
+
+global.scrollTo = jest.fn();
