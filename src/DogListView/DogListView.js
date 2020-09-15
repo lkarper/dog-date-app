6903b0b440017f7cc524c40fd3c6ae3aa7829b31
Luther Karper <luther.karper@live.com>
2020-09-15 19:36:51 -0400

    Add propTypes and defaultProps to DogListView

--- src/DogListView/DogListView.js
+++ src/DogListView/DogListView.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './DogListView.css';
 
 const DogListView = (props) => {
 
-    const { profile_img_url, name, id } = props.dog;
+    const { 
+        profile_img_url, 
+        name, 
+        id 
+    } = props.dog;
 
     const image = profile_img_url 
         ?
@@ -20,6 +25,18 @@ const DogListView = (props) => {
                 alt={`Avatar of the dog named ${name} not available.`} 
             />
     ;
+
+    if (!id) {
+        return (
+            <li className='DogListView__li'>
+                <p
+                    className='DogListView__p error'
+                >
+                    Error: Check your connection and the URL and try again.
+                </p>
+            </li>
+        );
+    }
 
     return (
         <li className='DogListView__li'>
@@ -40,6 +57,22 @@ const DogListView = (props) => {
             </Link>
         </li>
     );
+}
+
+DogListView.defaultProps = {
+    dog: {
+        profile_img_url: '', 
+        name: '', 
+        id: '' 
+    },
+};
+
+DogListView.propTypes = {
+    dog: PropTypes.shape({
+        profile_img_url: PropTypes.string, 
+        name: PropTypes.string,
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),         
+    }).isRequired,
 }
 
 export default DogListView;
