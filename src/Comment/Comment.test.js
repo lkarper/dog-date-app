8ebc56ea6beee7abd1166fd3430d9d2c7c4e11ae
Luther Karper <luther.karper@live.com>
2020-09-15 12:06:42 -0400

    Add tests for Comment component

--- src/Comment/Comment.test.js
+++ src/Comment/Comment.test.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { mount } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import AddCommentForm from './AddCommentForm';
+import Comment from './Comment';
 
-describe('AddCommentForm component', () => {
+describe('Comment component', () => {
     it('renders the UI without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(
-            <BrowserRouter>
-                <AddCommentForm />
-            </BrowserRouter>,
-            div
-        );
+        ReactDOM.render(<Comment />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
     it('renders the UI as expected', () => {
-        const wrapper = mount(
-            <BrowserRouter>
-                <AddCommentForm />
-            </BrowserRouter>
-        );
+        const wrapper = mount(<Comment />);
         expect(toJSON(wrapper)).toMatchSnapshot();
         wrapper.unmount();
     });
