ed683be2393d933c331f17ffb64ba964b50754df
Luther Karper <luther.karper@live.com>
2020-09-15 16:43:52 -0400

    Update setupTests.js
    Added a mock to render mapbox in tests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,3 +6,18 @@ configure({ adapter: new Adapter() });
 global.URL.createObjectURL = jest.fn();
 
 global.scrollTo = jest.fn();
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+    GeolocateControl: jest.fn(),
+    Map: jest.fn(() => ({
+      addControl: jest.fn(),
+      on: jest.fn(),
+      remove: jest.fn(),
+    })),
+    NavigationControl: jest.fn(),
+    Marker: jest.fn(() => ({
+        setLngLat: jest.fn(() => ({
+            addTo: jest.fn(),
+        })),
+    })),
+  }));
