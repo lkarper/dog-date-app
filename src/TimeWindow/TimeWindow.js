ee2e2e85126f387f06c784df5b77b9072e5a7a4e
Luther Karper <luther.karper@live.com>
2020-09-16 19:21:44 -0400

    Add defaultProps and propTypes to TimeWindow

--- src/TimeWindow/TimeWindow.js
+++ src/TimeWindow/TimeWindow.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TimeWindow = (props) => {
 
-    const { startTime, endTime } = props;
+    const { 
+        startTime, 
+        endTime 
+    } = props;
 
-    const displayStart = `${(parseInt(startTime.split(':')[0]) + 11) % 12 + 1}:${startTime.split(':')[1]} ${parseInt(startTime.split(':')[0]) >= 12 ? 'pm' : 'am'}`;
-    const displayEnd = `${(parseInt(endTime.split(':')[0]) + 11) % 12 + 1}:${endTime.split(':')[1]} ${endTime.split(':')[0] >= 12 ? 'pm' : 'am'}`;
+    if (!startTime || !endTime) {
+        return (
+            <p
+                className='error'
+            >
+                Error: Start or end time missing.
+            </p>
+        );
+    } else {
+        const displayStart = `${(parseInt(startTime.split(':')[0]) + 11) % 12 + 1}:${startTime.split(':')[1]} ${parseInt(startTime.split(':')[0]) >= 12 ? 'pm' : 'am'}`;
+        const displayEnd = `${(parseInt(endTime.split(':')[0]) + 11) % 12 + 1}:${endTime.split(':')[1]} ${endTime.split(':')[0] >= 12 ? 'pm' : 'am'}`;
 
-    return <p>{displayStart}{' - '}{displayEnd}</p>;
+        return <p>{displayStart}{' - '}{displayEnd}</p>;
+    }
+}
+
+TimeWindow.defaultProps = {
+    startTime: '',
+    endTime: '',
+};
+
+TimeWindow.PropTypes = {
+    startTime: PropTypes.string.isRequired,
+    endTime: PropTypes.string.isRequired,
 }
 
 export default TimeWindow;
