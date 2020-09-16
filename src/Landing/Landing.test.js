1174badaa65bc6c8619d229c6ee302820db715da
Luther Karper <luther.karper@live.com>
2020-09-16 13:41:36 -0400

    Add tests for Landing component

---
+++ src/Landing/Landing.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Landing from './Landing';
+
+describe('Landing component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <BrowserRouter>
+                <Landing />
+            </BrowserRouter>, 
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
