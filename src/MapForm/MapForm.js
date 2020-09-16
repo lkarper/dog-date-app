8115038f88c68672cf4905dbd94e6bffbb522469
Luther Karper <luther.karper@live.com>
2020-09-16 14:26:01 -0400

    Add defaultProps and propTypes to MapForm

--- src/MapForm/MapForm.js
+++ src/MapForm/MapForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Map from '../Map/Map';
 import CurrentLocation from '../CurrentLocation/CurrentLocation';
 import './MapForm.css';
 
 const MapForm = (props) => {
-    const { coordinates, setCoordinates } = props;
+    const { 
+        coordinates, 
+        setCoordinates, 
+    } = props;
 
     const [tempCoordinates, setTempCoordinates] = useState(coordinates);
 
@@ -31,5 +35,21 @@ const MapForm = (props) => {
         </fieldset>
     );
 }
+
+MapForm.defaultProps = {
+    coordinates: {
+        lat: 0,
+        lon: 0,
+    },
+    setCoordinates: () => {},
+};
+
+MapForm.propTypes = {
+    coordinates: PropTypes.shape({
+        lat: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        lon: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }).isRequired,
+    setCoordinates: PropTypes.func.isRequired,
+};
 
 export default MapForm;
