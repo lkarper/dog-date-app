463dfb19e210760176864267ce03db4665cabf5d
Luther Karper <luther.karper@live.com>
2020-09-15 21:21:59 -0400

    Add defaultProps & propTypes to DogReviewListItem

--- src/DogReviewListItem/DogReviewListItem.js
+++ src/DogReviewListItem/DogReviewListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import StarRating from '../StarRating/StarRating';
@@ -28,6 +29,16 @@ const DogReviewListItem = (props) => {
         profile_accuracy + 
         location_suitability 
     ) / 6;
+
+    if (!id) {
+        return (
+            <li
+                className='DogReviewListItem__li error'
+            >
+                <p>Error: could not load review info.  Check your connection and the URL and try again.</p>
+            </li>
+        )
+    }
 
     return (
         <li 
@@ -68,6 +79,38 @@ const DogReviewListItem = (props) => {
             </div>
         </li>
     );
+}
+
+DogReviewListItem.defaultProps = {
+    review: {
+        id: '',
+        review_title: '',
+        reviewer: '',
+        date_created: '',
+        friendliness_dogs: 0,
+        friendliness_people: 0,
+        playing_interest: 0,
+        obedience: 0,
+        profile_accuracy: 0,
+        location_suitability: 0,
+    },
+    noImage: false,
+};
+
+DogReviewListItem.propTypes = {
+    review: PropTypes.shape({
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        review_title: PropTypes.string,
+        reviewer: PropTypes.string,
+        date_created: PropTypes.string,
+        friendliness_dogs: PropTypes.number,
+        friendliness_people: PropTypes.number,
+        playing_interest: PropTypes.number,
+        obedience: PropTypes.number,
+        profile_accuracy: PropTypes.number,
+        location_suitability: PropTypes.number,
+    }).isRequired,
+    noImage: PropTypes.bool,
 }
 
 export default DogReviewListItem;
