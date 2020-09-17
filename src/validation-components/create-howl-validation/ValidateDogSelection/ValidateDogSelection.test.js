1e1cb82d339768569206be7803944b37909981e0
Luther Karper <luther.karper@live.com>
2020-09-17 11:52:56 -0400

    Add tests for ValidateDogSelection component

---
+++ src/validation-components/create-howl-validation/ValidateDogSelection/ValidateDogSelection.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import ValidateDogSelection from './ValidateDogSelection';
+
+describe('ValidateDogSelection component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ValidateDogSelection />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = mount(<ValidateDogSelection />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
+    });
+});
