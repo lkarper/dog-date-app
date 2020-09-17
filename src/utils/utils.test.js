0aa45cb3548eb2b89b56e482389cbca0f0cd863f
Luther Karper <luther.karper@live.com>
2020-09-16 21:40:49 -0400

    Add smoke tests for utils

---
+++ src/utils/utils.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { mount } from 'enzyme';
+import NativeClickListener from './NativeClickListener';
+import PublicOnlyRoute from './PublicOnlyRoute';
+import PrivateOnlyRoute from './PrivateOnlyRoute';
+
+const Test = () => <div />;
+const Test2 = () => <span />;
+
+describe('NativeClickListener component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <NativeClickListener>
+                <div />
+            </NativeClickListener>, 
+            div
+        );
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
+
+describe('PublicOnlyRoute component', () => {
+    describe('Given that there is no auth token', () => {
+        it('renders the UI without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(
+                <BrowserRouter>
+                    <PublicOnlyRoute
+                        path='/'
+                        component={Test}
+                    />
+                </BrowserRouter>,
+                div
+            );
+            ReactDOM.unmountComponentAtNode(div);
+        });
+    });
+});
+
+describe('PrivateOnlyRouter component', () => {
+    describe('Given that there is no auth token', () => {
+        it('redirects and renders the UI without crashing', () => {
+            const wrapper = mount(
+                <BrowserRouter>
+                    <Route 
+                        path='/login'
+                        component={Test2}
+                    />
+                    <PrivateOnlyRoute
+                        exact path='/'
+                        component={Test}
+                    />
+                </BrowserRouter>
+            );
+            wrapper.unmount();
+        });
+    });
+});
