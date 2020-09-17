2caaf77b55b174a6fb775c45d4446bb5dbadb715
Luther Karper <luther.karper@live.com>
2020-09-16 21:40:06 -0400

    Add defaultProps, propTypes to NativeClickListener

--- src/utils/NativeClickListener.js
+++ src/utils/NativeClickListener.js
@@ -1,8 +1,17 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+// This component fires a callback function whenever a click registers outside of its children prop
 
 const NativeClickListener = (props) => {
     
     const globalClickHandler = (nativeEvent) => {
+        
+        /* Checking whether the element outside of which the click registers 
+        has height prevents callbacks from firing if the element is not actually open.
+        This is primarily a concern for drop-down menus that are designed to close 
+        when the user clicks anywhere outside of them. */
+
         if (document.querySelector(`#${props.id}`).offsetHeight !== 0 && !document.querySelector(`#${props.id}`).contains(nativeEvent.target)) {
             return props.onClick(nativeEvent);
         }
@@ -25,5 +34,15 @@ const NativeClickListener = (props) => {
         </div>
     );
 }
+
+NativeClickListener.defaultProps = {
+    id: `default-${new Date().toJSON()}`,
+};
+
+NativeClickListener.propTypes = {
+    id: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.element.isRequired,
+};
 
 export default NativeClickListener;
