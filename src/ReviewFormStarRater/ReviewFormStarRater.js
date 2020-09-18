63172c987a4bf14ae4cc89c46bf406717194a39f
Luther Karper <luther.karper@live.com>
2020-09-18 15:47:40 -0400

    Update ReviewFormStarRater styles

--- src/ReviewFormStarRater/ReviewFormStarRater.js
+++ src/ReviewFormStarRater/ReviewFormStarRater.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import './ReviewFormStarRater.css';
 
 const ReviewFormStarRater = (props) => {
-
     const { 
         classSuffix, 
         currentState, 
         setter, 
         required,
-        legendText 
+        legendText,
     } = props;
 
     const [starRating, setStarRating] = useState(currentState.toString());
 
+    // Passes the currently checked radio box value to the parent component
     useEffect(() => {
         if (currentState !== starRating) {
             setter(starRating);
@@ -21,101 +21,86 @@ const ReviewFormStarRater = (props) => {
     }, [setter, currentState, starRating]);
 
     return (
-        <fieldset className="sub-fieldset">
+        <fieldset className='sub-fieldset'>
             <legend>{legendText}</legend>
-            <div className="ReviewFormStarRater__rate-container">                
+            <div className='ReviewFormStarRater__rate-container'>                
                 <input 
-                    className='ReviewFormStarRater__input'
-                    type="radio" 
+                    className={`ReviewFormStarRater__input `}
+                    type='radio' 
                     id={`star1-${classSuffix}`} 
                     name={`rate-${classSuffix}`} 
-                    value="1" 
+                    value='1' 
                     checked={starRating === '1'}
                     onChange={(e) => setStarRating(e.target.value)}
                     required={required}
                 />
                 <label
-                    className='ReviewFormStarRater__label'
+                    className={`ReviewFormStarRater__label ${starRating && parseInt(starRating) >= 1 ? 'auto-selected': 'not-selected'}`}
                     htmlFor={`star1-${classSuffix}`}
-                    style={{
-                        color: `${starRating && parseInt(starRating) >= 1 ? '#ffc700' : '#ccc'}`
-                    }}
                 >
                     1 star
                 </label>              
                 <input 
                     className='ReviewFormStarRater__input'
-                    type="radio" 
+                    type='radio' 
                     id={`star2-${classSuffix}`} 
                     name={`rate-${classSuffix}`} 
-                    value="2" 
+                    value='2' 
                     checked={starRating === '2'}
                     onChange={(e) => setStarRating(e.target.value)}
                     required={required}
                 />
                 <label 
-                    className='ReviewFormStarRater__label'
+                    className={`ReviewFormStarRater__label ${starRating && parseInt(starRating) >= 2 ? 'auto-selected': 'not-selected'}`}
                     htmlFor={`star2-${classSuffix}`}
-                    style={{
-                        color: `${starRating && parseInt(starRating) >= 2 ? '#ffc700' : '#ccc'}`
-                    }}
                 >
                     2 stars
                 </label>
                 <input 
                     className='ReviewFormStarRater__input'
-                    type="radio" 
+                    type='radio' 
                     id={`star3-${classSuffix}`} 
                     name={`rate-${classSuffix}`} 
-                    value="3" 
+                    value='3' 
                     checked={starRating === '3'}
                     onChange={(e) => setStarRating(e.target.value)}
                     required={required}
                 />
                 <label 
-                    className='ReviewFormStarRater__label'
+                    className={`ReviewFormStarRater__label ${starRating && parseInt(starRating) >= 3 ? 'auto-selected': 'not-selected'}`}
                     htmlFor={`star3-${classSuffix}`}
-                    style={{
-                        color: `${starRating && parseInt(starRating) >= 3 ? '#ffc700' : '#ccc'}`
-                    }}
                 >
                     3 stars
                 </label>
                 <input 
                     className='ReviewFormStarRater__input'
-                    type="radio" 
+                    type='radio' 
                     id={`star4-${classSuffix}`} 
                     name={`rate-${classSuffix}`} 
-                    value="4" 
+                    value='4' 
                     checked={starRating === '4'}
                     onChange={(e) => setStarRating(e.target.value)}
                     required={required}
                 />
                 <label 
-                    className='ReviewFormStarRater__label'
+                    className={`ReviewFormStarRater__label ${starRating && parseInt(starRating) >= 4 ? 'auto-selected': 'not-selected'}`}
                     htmlFor={`star4-${classSuffix}`}
-                    style={{
-                        color: `${starRating && parseInt(starRating) >= 4 ? '#ffc700' : '#ccc'}`
-                    }}
                 >
                     4 stars
                 </label>
                 <input 
                     className='ReviewFormStarRater__input'
-                    type="radio" 
+                    type='radio' 
                     id={`star5-${classSuffix}`} 
                     name={`rate-${classSuffix}`} 
-                    value="5" 
+                    value='5' 
                     checked={starRating === '5'}
                     onChange={(e) => setStarRating(e.target.value)}
                     required={required}
                 />
                 <label 
-                    className='ReviewFormStarRater__label'
+                    className={`ReviewFormStarRater__label ${starRating && parseInt(starRating) === 5 ? 'auto-selected': 'not-selected'}`}
                     htmlFor={`star5-${classSuffix}`}
-                    style={{
-                        color: `${starRating && parseInt(starRating) === 5 ? '#ffc700' : '#ccc'}`
-                    }}
                 >
                     5 stars
                 </label>                
