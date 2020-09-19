0333a07b9784310446386814429721a2bf1bbcd3
Luther Karper <luther.karper@live.com>
2020-09-19 12:28:02 -0400

    Add comment

--- src/validation-components/registration-validation/ValidatePhoneNumber/ValidatePhoneNumber.js
+++ src/validation-components/registration-validation/ValidatePhoneNumber/ValidatePhoneNumber.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+// Phone number is optional, but if provided must be validated to ensure proper formatting
 const ValidatePhoneNumber = (props) => {
     const { 
         phone, 
         phoneValidationError,
-        setPhoneValidationError 
+        setPhoneValidationError, 
     } = props;
 
     const [phoneValidationMessage, setPhoneValidationMessage] = useState(`Phone number optional, but if provided must match format '123-456-7890'`);
 
     useEffect(() => {
-
         const regex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
         const validPhone = regex.test(phone);
 
