660fd83034823d3036415d3ce16010a5ce9edd01
Luther Karper <luther.karper@live.com>
2020-09-19 10:53:17 -0400

    Add comments

--- src/StarRating/StarRating.js
+++ src/StarRating/StarRating.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './StarRating.scss';
 
-const StarRating = (props) => {
-    
+const StarRating = (props) => {    
     const { rating } = props;
 
     return (
         <div 
             className="StarRating__stars" 
+            /* 
+                Passes the --rating variable to .scss file; 
+                this variable is used to create the star rating graphic.
+            */
             style={{ '--rating': `${rating}` }} 
             aria-label={`Rating is ${rating} out of 5.`}
         />
