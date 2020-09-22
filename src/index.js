be58e6da3ebe2b05beaef8c5759042f641218f9e
Luther Karper <luther.karper@live.com>
2020-09-21 21:13:05 -0400

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
+import 'normalize.css';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
